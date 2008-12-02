b27b506b34532f776f3a5ae237792192761e547e
Sam Rushing <gitsam@rushing.nightmare.com>
2008-12-02 03:55:18 +0000

    don't tar up the svn dir
    git-svn-id: file:///Users/rushing/svn/pxll@7 6687424e-f796-406d-b1dd-24293e5947de

--- util/dist.py
+++ util/dist.py
@@ -7,6 +7,6 @@ execfile ('util/clean.py')
 os.chdir ('..')
 tarball = 'pxll.%s.tar.gz' % (time.strftime ('%y%m%d'),)
 # COPYFILE_DISABLE is a hack to turn off storing resource forks in Leopard.
-os.system ('COPYFILE_DISABLE=true tar -zcvf %s pxll' % (tarball,))
+os.system ('COPYFILE_DISABLE=true tar --exclude .svn -zcvf %s pxll' % (tarball,))
 os.system ('scp %s dark:public_html/pxll/' % (tarball,))
 os.chdir ('pxll')
