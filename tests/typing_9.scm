ce4e7dd1e4e5261b61fbc20a39d1c3112d8c8d65
Sam Rushing <gitsam@rushing.nightmare.com>
2008-12-04 06:43:04 +0000

    test assignment
    git-svn-id: file:///Users/rushing/svn/pxll@18 6687424e-f796-406d-b1dd-24293e5947de

--- tests/typing_9.scm
+++ tests/typing_9.scm
@@ -2,7 +2,11 @@
 (define (printn x)
   (%%cexp (? -> undefined) "dump_object (%s, 0); fprintf (stdout, \"\\n\")" x))
 
-(let ((o1 #(1 2 3)))
+(let ((o1 #(1 2 3))
+      (o2 #("hello" "there"))
+      )
   (printn o1[0])
-  (printn o1[5])
+  (set! o1[1] 5)
+  (printn o1)
+  (printn o2)
   )
