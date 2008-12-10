1ecb7aa1758068a6271df8ca08c4fd8fe0eccbf6
Sam Rushing <gitsam@rushing.nightmare.com>
2008-12-10 23:02:15 +0000

    first test of full algebraic datatypes
    git-svn-id: file:///Users/rushing/svn/pxll@37 6687424e-f796-406d-b1dd-24293e5947de

---
+++ tests/sumtypes_1.scm
@@ -0,0 +1,33 @@
+
+(define (printn x)
+  (%%cexp (? -> undefined) "dump_object (%s, 0); fprintf (stdout, \"\\n\")" x))
+
+(define (+ x y)
+  (%%cexp (int int -> int) "%s+%s" x y))
+
+(define (* x y)
+  (%%cexp (int int -> int) "%s*%s" x y))
+
+(datatype exp
+  (union 
+    (literal int)
+    (sum exp exp)
+    (prod exp exp)
+    ))
+
+(define (exp-value n:exp)
+  (typecase n
+    ((literal v) v)
+    ((sum a b) (+ (exp-value a) (exp-value b)))
+    ((prod a b) (* (exp-value a) (exp-value b)))))
+
+(let* ((x (exp/literal 19))
+       (y (exp/sum x x))
+       (z (exp/prod y y))
+       )
+  (printn x)
+  (printn y)
+  (printn z)
+  (printn (exp-value z))
+  )
+
