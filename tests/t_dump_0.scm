07570d559e6585311aa6a913dbb3f9109762f8c6
Sam Rushing <gitsam@rushing.nightmare.com>
2008-12-22 21:16:34 +0000

    updated for new typing
    git-svn-id: file:///Users/rushing/svn/pxll@104 6687424e-f796-406d-b1dd-24293e5947de

--- tests/t_dump_0.scm
+++ tests/t_dump_0.scm
@@ -1,20 +1,14 @@
 (include "lib/core.scm")
-(include "lib/pair.scm")
 (include "lib/string.scm")
 
-(define (dump filename thunk)
-  (%%verify "TC_STRING" 1 filename)
-  (%%cexp "dump_image (GET_STRING_POINTER(%s), %s)" filename thunk)
-  #f)
-
-(define (load filename)
-  (%printn "calling load()...")
-  (%%cexp "load_image (GET_STRING_POINTER(%s))" filename))
-
 (define (thingy)
-  (%printn "howdy there!\n"))
+  (printn "howdy there!\n")
+  ;; thunks must return an integer
+  0
+  )
 
-(%printn (sys.argv 0))
-(if (and (> (sys.argc) 1) (string-=? (sys.argv 1) "-d"))
+(printn (sys:argv 0))
+(if (and (> (sys:argc) 1) (string-=? (sys:argv 1) "-d"))
     (dump "test.img" thingy)
     ((load "test.img")))
+
