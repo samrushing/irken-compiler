e39ddbf1c0b91ddd3ddc378e5730e50853d6afd0
Sam Rushing <gitsam@rushing.nightmare.com>
2008-12-22 20:29:37 +0000

    pxll=>irken
    git-svn-id: file:///Users/rushing/svn/pxll@82 6687424e-f796-406d-b1dd-24293e5947de

--- util/dist.py
+++ util/dist.py
@@ -5,8 +5,8 @@ import time
 
 execfile ('util/clean.py')
 os.chdir ('..')
-tarball = 'pxll.%s.tar.gz' % (time.strftime ('%y%m%d'),)
+tarball = 'irken.%s.tar.gz' % (time.strftime ('%y%m%d'),)
 # COPYFILE_DISABLE is a hack to turn off storing resource forks in Leopard.
-os.system ('COPYFILE_DISABLE=true tar --exclude .svn -zcvf %s pxll' % (tarball,))
-os.system ('scp %s dark:public_html/pxll/' % (tarball,))
-os.chdir ('pxll')
+os.system ('COPYFILE_DISABLE=true tar --exclude .svn -zcvf %s irken' % (tarball,))
+os.system ('scp %s dark:public_html/irken/' % (tarball,))
+os.chdir ('irken')
