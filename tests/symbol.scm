ae6759d1babe65a6c2dc5c6ff1d829672ba75dd7
Sam Rushing <gitsam@rushing.nightmare.com>
2008-12-22 21:15:57 +0000

    updated for new typing
    git-svn-id: file:///Users/rushing/svn/pxll@101 6687424e-f796-406d-b1dd-24293e5947de

--- tests/symbol.scm
+++ tests/symbol.scm
@@ -1,31 +1,12 @@
 
 (include "lib/core.scm")
-(include "lib/vector.scm")
-(include "lib/pair.scm")
 (include "lib/string.scm")
 (include "lib/frb.scm")
+(include "lib/symbol.scm")
 
-(define (make-string-equal?-tree)
-  (make-tree string-<? #f))
-
-(define the-symbol-table #f)
-
-(define (string->uninterned-symbol str)
-  (let ((sym (%make-tuple #x1c 1)))
-    (%%cexp "%s[1] = %s" sym str)
-    sym))
-
-(define (string->symbol str)
-  (let ((probe (tree-member the-symbol-table str)))
-    (if probe
-	probe
-	(let ((sym (string->uninterned-symbol str)))
-	  (set! the-symbol-table (tree-insert the-symbol-table str sym))
-	  sym))))
-
-(set! the-symbol-table (make-string-equal?-tree))
-
-(let ((sym0 (string->symbol "hello"))
-      (sym1 (string->symbol "hello")))
-  (%printn (%eq? sym0 sym1)))
-
+(printn (string->symbol "howdy"))
+(printn (string->symbol "howdy"))
+(printn (string->symbol "yowsa"))
+(printn (string->symbol "hooga"))
+(printn the-symbol-table)
+(printn 'thingy)
