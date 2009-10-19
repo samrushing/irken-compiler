007cc6324d84c1426f4d8e5c79aa57c65da1d8e9
Sam Rushing <gitsam@rushing.nightmare.com>
2009-10-19 20:44:39 +0000

    clearing out mods in prep for the new type solver

--- lib/io.scm
+++ lib/io.scm
@@ -1,17 +1,14 @@
 ;; -*- Mode: Scheme -*-
 
-(define (open path flags)
-  (%%verify "TC_STRING" 1 path)
-  (%%verify "TC_INT" 1 flags)
-  (let ((fd (%%cexp "box (open (GET_STRING_POINTER(%s), unbox(%s)))" path flags)))
-    (if (%ge? fd 0)
+(define (open path oflag mode)
+  (let ((fd (%%cexp (string int int -> int) "open (%s, %s, %s)" path oflag mode)))
+    (if (>= fd 0)
 	fd
 	(error "open() failed"))))
 
 (define (read fd size)
-  (%%verify "TC_INT" 1 fd)
   (let* ((buffer (make-string size))
-	 (r (%%cexp "box (read (unbox(%s), GET_STRING_POINTER(%s), unbox(%s)))" fd buffer size)))
+	 (r (%%cexp (int string int -> int) "read (%s, %s, %s)" fd buffer size)))
     (if (= r size)
 	buffer
 	(if (< r size)
@@ -19,137 +16,53 @@
 	    (error "read() failed")))))
 
 (define (read-into-buffer fd buffer)
-  (%%verify "TC_INT" 1 fd)
   (let* ((size (string-length buffer))
-	 (r (%%cexp "box (read (unbox(%s), GET_STRING_POINTER(%s), unbox(%s)))" fd buffer size)))
+	 ;; XXX range check
+	 (r (%%cexp (int string int -> int) "read (%s, %s, %s)" fd buffer size)))
     r))
 
 (define (write fd s)
-  (%%verify "TC_INT" 1 fd)
-  (%%verify "TC_STRING" 1 s)
-  (%%cexp "box (write (unbox(%s), GET_STRING_POINTER(%s), unbox(%s)))" fd s (string-length s)))
+  (%%cexp (int string int -> int) "write (%s, %s, %s)" fd s (string-length s)))
 
 (define (write-substring fd s start len)
-  ;; XXX range check!
-  (%%verify "TC_INT" 1 fd)
-  (%%verify "TC_STRING" 1 s)
-  (%%verify "TC_INT" 1 start)
-  (%%verify "TC_INT" 1 len)
-  (%%cexp "box (write (unbox(%s), GET_STRING_POINTER(%s)+unbox(%s), unbox(%s)))" fd s start len))
+  ;; XXX range check
+  (%%cexp (int string int int -> int) "write (%s, %s+%s, %s)" fd s start len))
 
 (define (close fd)
-  (%%cexp "box (close (unbox (%s)))" fd))
+  (%%cexp (int -> int) "close (%s)" fd))
 
-(define eof-object 'end-of-file)
+(class readfile
 
-;; this message-based OO will do until we get to the plans in lib/uobj.scm
+  (fd:int buffer:string pos:int end:int)
 
-(define (buffered-file fd buffer-size)
+  (define (fill-buffer self)
+    (let ((n (read-into-buffer self.fd self.buffer)))
+      (set! self.end n)
+      (set! self.pos 0)
+      n))
+	     
+  (define (read-buffer self)
+    ;; get an entire buffer-full at a time
+    (cond ((< self.pos self.end)
+	   (let ((opos self.pos))
+	     (set! self.pos self.end)
+	     (substring self.buffer opos self.end)))
+	  ((= (self.read-buffer) 0) "")
+	  (else
+	   (set! self.end 0)
+	   (set! self.pos 0)
+	   self.buffer)))
 
-  ;; meant to be used for *either* reading *or* writing, not both.   ya dingbat.
-
-  ;; [-----<pos>XXXXXXXXX<end>------]
-  
-  (let ((buffer (make-string buffer-size))
-        (pos 0)
-        (end 0))
-
-    (define (read-buffer)
-      (let ((n (read-into-buffer fd buffer)))
-	;;(print-string "read ") (%print n) (print-string " bytes into buffer\n")
-        (cond ((= n 0) eof-object)
-              ((> n 0)
-               (set! end n)
-               (set! pos 0))
-              (else
-               (error "read(2) failed")))))
-
-    (define (read-buffer*)
-      ;; get an entire buffer-full at a time
-      (cond ((< pos end)
-	     (let ((opos pos))
-	       (set! pos end)
-	       (substring buffer opos end)))
-	    ((eq? (read-buffer) eof-object)
-	     eof-object)
-	    (else
-	     (read-buffer*))))
-
-    (define (write-buffer)
-      (let loop ()
-        (if (< pos end)
-            (let ((n (write-substring buffer fd pos (- end pos))))
-	      (cond ((> n 0)
-		     (set! pos (+ pos n))
-		     (loop))
-		    (else
-		     (error "write(2) failed"))))))
-      (set! pos 0)
-      (set! end 0)
-      )
-
-    (define (read-char)
-      (cond ((< pos end)
-             (let ((ch (string-ref buffer pos)))
-               (set! pos (+ pos 1))
-               ch))
-            (else
-             (if (eq? (read-buffer) eof-object)
-		 eof-object
-                 (read-char)))))
-
-    (define (read-line)
-      (let oloop ((buffers '()))
-	(let iloop ((i pos))
-	  (cond ((= i end)
-		 ;; fetch a new buffer
-		 (let ((piece (substring buffer pos end)))
-		   (read-buffer)
-		   (oloop (cons piece buffers))))
-		((eq? (string-ref buffer i) #\newline)
-		 (let ((result (substring buffer pos i)))
-		   (set! pos i)
-		   (reverse (cons result buffers))))
-		(else
-		 (iloop (+ i 1)))))))
-
-    (define (write-char ch)
-      (cond ((< end buffer-size)
-             (string-set! buffer end ch)
-             (set! end (+ end 1)))
-            (else
-             (write-buffer)
-             (write-char ch))))
-
-    (define (write-string s)
-      (let ((sl (string-length s)))
-        (cond ((> (+ sl end) buffer-size)
-               ;; just flush then write the string
-               (write-buffer)
-               (write fd s))
-              (else
-               (buffer-copy s 0 sl buffer end)
-               (set! end (+ end sl))))))
-
-    (define (close*)
-      (flush)
-      ;; little issue with names here, should probably call
-      ;; the direct os interfaces os-open, os-read, etc...
-      (close fd))
-
-    (define (flush)
-      (write-buffer))
-
-    ;; simple message dispatcher
-    (lambda (msg)
-      (case msg
-	((read-char) read-char)
-	((read-buffer) read-buffer*)
-	((write-char) write-char)
-	((write-string) write-string)
-	((write-buffer) write-buffer)	
-	((flush) flush)
-	((close) close*)
-	(else (error "no such method"))))
-    )
+  (define (read-char self)
+    (cond ((< self.pos self.end)
+	   (set! self.pos (+ self.pos 1))
+	   (maybe/yes (string-ref self.buffer (- self.pos 1))))
+	  ((= (self.read-buffer) 0)
+	   (maybe/no))
+	  (else
+	   (self.read-char))))
   )
+
+(define (open-readfile path buffer-size)
+  (readfile (open path 0 0) (make-string buffer-size) 0 0))
+
