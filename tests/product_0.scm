d484d10098a243dab8e39b9d9b85940ea3c8d6fa
Sam Rushing <gitsam@rushing.nightmare.com>
2009-10-19 20:44:07 +0000

    clearing out mods in prep for the new type solver

--- tests/product_0.scm
+++ tests/product_0.scm
@@ -1,8 +1,11 @@
 
 (include "lib/core.scm")
 
-(datatype pair
-  (product ? ? ))
+;(datatype pair
+;  (product ? ? ))
+
+;(define (add-pair p:pair)
+;  (+ p{0} p{1}))
 
 (let ((p0 (pair 1 2))
       (p1 (pair #t #f))
@@ -13,8 +16,11 @@
   (printn p1)
   (printn p2)
   (printn a0[0])
-  (printn p2[0])
+  (printn p2{0})
   ;; will fail
-  ;; (printn p2[5])
-  (printn p2[1])
+  ;; (printn p2{5})
+  (printn p2{1})
+  (printn (+ a0[0] a0[1]))
+  (printn (+ p1{0} p1{1}))
+;  (printn (add-pair p0))
   )
