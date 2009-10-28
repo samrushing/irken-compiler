e50c6200db49153b3f6ee56005e8b8ad18260f68
Sam Rushing <gitsam@rushing.nightmare.com>
2009-10-28 23:27:15 +0000

    use %%cexp

--- tests/t0.scm
+++ tests/t0.scm
@@ -1,5 +1,16 @@
+(define (= a b)
+  (%%cexp (int int -> bool) "%s==%s" a b))
+
+(define (+ a b)
+  (%%cexp (int int -> int) "%s+%s" a b))
+
+(define (- a b)
+  (%%cexp (int int -> int) "%s-%s" a b))
+
+(define (ident x) x)
+
 (let loop ((n 100)
 	   (a 0))
-  (if (%zero? n)
+  (if (= n 0)
       a
-      (loop (%- n 1) (%+ a n))))
+      (loop (- n 1) (+ a n))))
