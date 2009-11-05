533894d5db78023e2bf664917941c05a3e60d779
Sam Rushing <gitsam@rushing.nightmare.com>
2009-11-05 22:38:17 +0000

    playing with new vcase syntax

--- tests/t8.scm
+++ tests/t8.scm
@@ -3,11 +3,10 @@
 (define (+ a b)
   (%%cexp (int int -> int) "%s+%s" a b))
 
-(let ((x (%vcon/fnord 12))
-      (y (%vcon/blort #f))
-      )
-  (vcase x
-    ((fnord a) (+ a 5))
-    ((blort b) (if b 9 8))
+(let ((x (:fnord 12))
+      (y (:blort #f)))
+  (vcase y
+    ((:fnord a) (+ a 5))
+    ((:blort b) (if b 9 8))
+    (else 12)
     ))
-  
