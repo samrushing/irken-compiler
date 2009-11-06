ec85f292b555294f914af68226de299d8d862f5b
Sam Rushing <gitsam@rushing.nightmare.com>
2009-11-06 05:44:58 +0000

    test the speed of record and variant creation

---
+++ tests/t9.scm
@@ -0,0 +1,17 @@
+(define (- a b)
+  (%%cexp (int int -> int) "%s-%s" a b))
+
+(define (= a b)
+  (%%cexp (int int -> bool) "%s==%s" a b))
+
+(define (zero? x)
+  (%%cexp (int -> bool) "%s==0" x))
+
+(let loop ((n 1000000)
+	   (z {thing=0 blorb=#f})
+	   (y (:blort 12)))
+  (if (zero? n)
+      z
+      (loop (- n 1)
+	    {thing=n blorb=#f}
+	    (:blort n))))
