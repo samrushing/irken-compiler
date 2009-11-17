1d4c1e1eba765c1e56dd52787df8bd7e4047d2b2
Sam Rushing <gitsam@rushing.nightmare.com>
2009-11-17 01:14:43 +0000

    test the symbol table

---
+++ tests/t19.scm
@@ -0,0 +1,18 @@
+(include "lib/core.scm")
+;(include "lib/frb.scm")
+(include "lib/pair.scm")
+(include "lib/string.scm")
+;(include "lib/symbol.scm")
+
+;; (printn (string-length "howdyx"))
+;; (printn (string-compare "howdy" "howdyx"))
+;; (printn (string-compare "howdyx" "howdy"))
+;; (printn (string-compare "abc" "defghijkl"))
+;; (printn (string-compare "defghijkl" "abc"))
+
+;; (printn (string->symbol "howdy"))
+;; (printn (string->uninterned-symbol "howdy"))
+;; (printn (string->symbol "testing"))
+;; (printn #('florb 'testing 'grok 'urk))
+
+(list->string (string->list "sam"))
