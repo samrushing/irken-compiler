f8eb5c2d68a59c5ada0891c7d10ffecaea3a5323
Sam Rushing <gitsam@rushing.nightmare.com>
2009-11-17 01:09:28 +0000

    updated for the new type system

--- lib/symbol.scm
+++ lib/symbol.scm
@@ -2,17 +2,17 @@
 (define (string->uninterned-symbol str)
   (%%make-tuple symbol symbol str))
 
-(define the-symbol-table (node/empty))
+(define the-symbol-table (:empty))
 
 (define (intern-symbol str)
   (let ((sym (string->uninterned-symbol str)))
     (set! the-symbol-table
-	  (node:insert the-symbol-table string-<? str sym))
+	  (tree:insert the-symbol-table string-<? str sym))
     sym))
 
 (define (string->symbol str)
-  (let ((probe (node:member the-symbol-table string-<? str)))
-    (typecase maybe probe
-      ((no) (intern-symbol str))
-      ((yes sym) sym))))
+  (let ((probe (tree:member the-symbol-table string-<? str)))
+    (vcase probe
+      ((:no) (intern-symbol str))
+      ((:yes sym) sym))))
 
