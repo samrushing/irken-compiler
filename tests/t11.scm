5a59065ef846645464189be8a251751e3a9d358a
Sam Rushing <gitsam@rushing.nightmare.com>
2009-11-30 04:45:15 +0000

    give a good workout to variant case, including don't-care binding, inlining, etc..

--- tests/t11.scm
+++ tests/t11.scm
@@ -1,9 +1,31 @@
+;; give a good workout to variant case, including don't-care binding, inlining, etc..
+
+(define (printn x)
+  (%%cexp ('a -> undefined) "dump_object (%s, 0); fprintf (stdout, \"\\n\")" x))
+
 (define (+ a b)
   (%%cexp (int int -> int) "%s+%s" a b))
 
-(let ((x (:blurb))
-      (y (:blort 12 13 14)))
-  (vcase y
+(define (glort0 a)
+  (vcase a
      ((:blurb) 20)
-     ((:blort a b c) (+ a (+ b c)))))
-     
+     ((:blort a b c) (+ a (+ b c)))
+     ((:urk) 666)))
+    
+(define (glort1 a)
+  (vcase a
+     ((:blurb) 20)
+     ((:blort _ _ _ ) 42)
+     ((:urk) 666)))
+
+(let ((x (:blurb))
+      (y (:blort 12 13 14))
+      (z (:urk))
+      )
+  (printn (glort0 x))
+  (printn (glort0 y))
+  (printn (glort0 z))
+  (printn (glort1 x))
+  (printn (glort1 y))
+  (printn (glort1 z))
+  )
