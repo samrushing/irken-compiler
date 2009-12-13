c2cee5fa46c070cb8e7658e67563cbee9823defd
Sam Rushing <gitsam@rushing.nightmare.com>
2009-12-13 21:30:58 +0000

    make sure empty vectors (i.e., TC_EMPTY_VECTOR) work

---
+++ tests/t_empty_vector.scm
@@ -0,0 +1,23 @@
+
+(include "lib/core.scm")
+(include "lib/pair.scm")
+
+(let ((z (n-of 100 (:testing 23 #f))))
+  ;; z gives the gc something to do
+
+  (let loop ((n 10000))
+    (let ((v (%make-vector 0 0)))
+      (if (= n 0)
+	  v
+	  (loop (- n 1)))))
+  
+  (let loop ((n 100000))
+    (let ((x #(#() #() #() #() #() #(1 2 3))))
+      (set! x[5] (%make-vector 0 3))
+      (set! x[0] (%make-vector 0 4))
+      )
+    (if (= n 0)
+	#t
+	(loop (- n 1))))
+  )
+
