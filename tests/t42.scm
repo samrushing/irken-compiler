00f2b41fc485a8350756118287866e428e31edca
Sam Rushing <gitsam@rushing.nightmare.com>
2009-12-13 01:02:44 +0000

    removed prototype code

--- tests/t42.scm
+++ tests/t42.scm
@@ -1,27 +1,6 @@
 
 (include "lib/core.scm")
 
-(define (my-make-generator producer)
-  (let ((ready #f)
-        ;; just holding useless continuations
-        (caller (getcc))
-        (saved-point (getcc)))
-
-    (define (entry-point)
-      (set! caller (getcc))
-      (if ready
-	  (putcc saved-point #f)
-	  (producer yield)))
-
-    (define (yield v)
-      (set! saved-point (getcc))
-      (set! ready #t)
-      (putcc caller v))
-
-    entry-point
-
-    ))
-
 (define (make-int-generator n)
   (make-generator
    (lambda (consumer)
