a575fa9586ce83065f1ea64b6011abc47f4f001f
Sam Rushing <gitsam@rushing.nightmare.com>
2010-02-24 18:04:09 +0000

    redone with a datatype declaration

--- lib/pair.scm
+++ lib/pair.scm
@@ -1,24 +1,29 @@
 
+(datatype list
+  (cons 'a list)
+  (nil)
+  )
+
 (define (null? l)
-  (vcase l
+  (vcase list l
     ((:nil) #t)
     ;; could/should use 'else' here.
     ((:cons _ _ ) #f)))
 
 (define (cons a b)
-  (:cons a b))
+  (list:cons a b))
 
 ;; http://groups.google.com/group/comp.lang.scheme/msg/0055f311d1e1ce08
 
 (define (reverse-onto l1 l2)
-  (vcase l1
+  (vcase list l1
     ((:nil) l2)
     ((:cons hd tl)
-     (reverse-onto tl (:cons hd l2)))
+     (reverse-onto tl (list:cons hd l2)))
     ))
 
 (define (reverse l)
-  (reverse-onto l (:nil)))
+  (reverse-onto l (list:nil)))
 
 (define (append list1 list2)
   (reverse-onto (reverse list1) list2))
@@ -26,13 +31,13 @@
 (define (length l)
   (let loop ((l l)
 	     (n 0))
-    (vcase l
+    (vcase list l
        ((:nil) n)
        ((:cons _ tl)
 	(loop tl (+ n 1))))))
 
 (define (member x l =)
-  (vcase l
+  (vcase list l
     ((:nil) #f)
     ((:cons hd tl)
      (if (= hd x)
@@ -41,14 +46,14 @@
 
 (define (range n)
   (let loop ((n n)
-	     (l (:nil)))
+	     (l (list:nil)))
     (if (= n 0)
 	l
 	(loop (- n 1) (cons n l)))))
 
 (define (n-of n x)
   (let loop ((n n)
-	     (l (:nil)))
+	     (l (list:nil)))
     (if (= n 0)
 	l
 	(loop (- n 1) (cons x l)))))
