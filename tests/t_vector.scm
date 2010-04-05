2d7cdcd7105307fcdb93de682c19030d708f20d1
Sam Rushing <gitsam@rushing.nightmare.com>
2010-04-05 19:08:59 +0000

    tests vector->list and list->vector

--- tests/t_vector.scm
+++ tests/t_vector.scm
@@ -1,4 +1,10 @@
 
+(include "lib/core.scm")
+(include "lib/pair.scm")
+
 (define (thing v) v[0])
 
-(thing #(1 2 3))
+(printn (thing #(1 2 3)))
+(printn (vector-length #(1 2 3 4 5)))
+(printn (vector->list #(1 2 3 4 5)))
+(printn (list->vector (vector->list #(1 2 3 4 5))))
