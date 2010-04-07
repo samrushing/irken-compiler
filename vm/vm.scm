2ab657048d3b4fdffc6b0d1145c767d4f808706e
Sam Rushing <gitsam@rushing.nightmare.com>
2010-04-07 21:40:38 +0000

    simpler vector-of-ints VM model +1,+2,etc..: performance/readability hack datatype object: an efficient type-safe wrapper for VM objects! read-int(): encoding changed to favor single-byte values

--- vm/vm.scm
+++ vm/vm.scm
@@ -5,13 +5,17 @@
 (include "lib/string.scm")
 (include "lib/io.scm")
 
-(define (unbox x)
-  ;; type-unsafe - allows us to 'cast' a literal so its hidden
-  ;;   run-time type is revealed.
-  (%%cexp (int -> 'a) "/*cast*/(object*)%s" x))
-
-(datatype insn-stream
-  (:t (list (-> int)) (list int)))
+(datatype object
+  (:int int)
+  (:char char)
+  (:bool bool)
+  (:string string)
+  (:undefined undefined)
+  (:symbol symbol)
+  (:continuation continuation)
+  ;; (:closure ...)
+  ;; (:lenv ...)
+  )
 
 (define (CONS a b) (list:cons a b))
 (define (NIL) (list:nil))
@@ -32,112 +36,125 @@
     (define (nexti)
       (char->ascii (next)))
 
-    (define (read-lit)
-      (let loop ((n 0) (len (char->ascii (next))))
-	(if (= len 0)
-	    n
-	    (loop (+ (<< n 8) (char->ascii (next))) (- len 1)))))
+    (define (read-int)
+      (let ((n (char->ascii (next))))
+	(if (= n 255)
+	    (let loop ((n 0) (len (char->ascii (next))))
+	      (if (= len 0)
+		  n
+		  (loop (+ (<< n 8) (char->ascii (next))) (- len 1))))
+	    n)))
 
-    (let loop ((insns (NIL))
-	       (data (NIL)))
-      (match (next) with
-	#\L ;; literal
-	-> (let ((target (nexti))
-		 (value (unbox (read-lit))))
-	     (loop (CONS insn-literal insns)
-		   (CONS value (CONS target data))))
+    (let ((lits
+	   ;; read vector of literals
+	   (let loop ((lits (NIL)))
+	     (match (next) with
+	       #\+ -> (loop (CONS (object:int (read-int)) lits))
+	       #\- -> (loop (CONS (object:int (- 0 (read-int))) lits))
+	       #\T -> (loop (CONS (object:bool #t) lits))
+	       #\F -> (loop (CONS (object:bool #f) lits))
+	       #\. -> (reverse lits)
+	       _ -> (error "reading lits")
+	       ))))
 
-	#\R ;; return
-	-> (insn-stream:t (reverse (CONS insn-return insns)) (reverse (CONS (nexti) data)))
+      (let loop ((stream (NIL)))
+	(if (= pc (string-length code))
+	    { code = (reverse stream) lits = lits }
+	    (loop (CONS (read-int) stream))))
+      )))
 
-	#\+ ;; primop plus
-	-> (let ((target (nexti))
-		 (r0 (nexti))
-		 (r1 (nexti)))
-	     (loop (CONS insn-plus insns)
-		   (CONS r1 (CONS r0 (CONS target data)))))
-
-	#\- ;; primop minus
-	-> (let ((target (nexti))
-		 (r0 (nexti))
-		 (r1 (nexti)))
-	     (loop (CONS insn-minus insns)
-		   (CONS r1 (CONS r0 (CONS target data)))))
-
-	#\= ;; primop equals
-	-> (let ((target (nexti))
-		 (r0 (nexti))
-		 (r1 (nexti)))
-	     (loop (CONS insn-equals insns)
-		   (CONS r1 (CONS r0 (CONS target data)))))
-
-	_ -> (error "bytecode")
-
-	))
-    ))
-
-;; insn thunks
-(define vm-insns #(insn-return))
-;; insn data
-(define vm-data #(0))
+;; performance hacks
+(define (+1 a)
+  (%%cexp (int -> int) "%s+1" a))
+(define (+2 a)
+  (%%cexp (int -> int) "%s+2" a))
+(define (+3 a)
+  (%%cexp (int -> int) "%s+3" a))
+(define (+4 a)
+  (%%cexp (int -> int) "%s+4" a))
 
 ;; VM registers
-(define regs #(0 0 0 0 0 0 0 0 0 0))
+(define ZED (object:int 0))
+(define regs #(ZED ZED ZED ZED ZED ZED ZED ZED ZED ZED))
 
-(define pci 0)
-(define pcd 0)
+(define pc 0)
 
 (define (next-insn)
-  (set! pci (+ pci 1))
-  vm-insns[pci]
+  (OPS[CODE[pc]])
   )
 
-(define (next-data)
-  (let ((r vm-data[pcd]))
-    (set! pcd (+ pcd 1))
-    r))
+(define (insn-lit)
+  (printn "lit")
+  (set! regs[CODE[(+1 pc)]] LITS[CODE[(+2 pc)]])
+  (set! pc (+3 pc))
+  (printn regs)
+  (next-insn))
 
-(define (insn-literal)
-  (set! regs[vm-data[pcd]] vm-data[(+ pcd 1)])
-  (set! pcd (+ pcd 2))
-  ((next-insn)))
-
-(define (insn-return)
-  regs[vm-data[pcd]]
+(define (insn-ret)
+  (printn "ret")
+  (printn pc)
+  regs[CODE[(+1 pc)]]
   )
 
-(define (insn-plus)
-  (set! regs[vm-data[pcd]] (+ regs[vm-data[(+ pcd 1)]] regs[vm-data[(+ pcd 2)]]))
-  (set! pcd (+ pcd 3))
-  ((next-insn)))
+(define (insn-add)
+  (printn "add")
+  (match regs[CODE[(+2 pc)]] regs[CODE[(+3 pc)]] with
+    (object:int x) (object:int y) -> (begin (set! regs[CODE[(+1 pc)]] (object:int (+ x y)))
+					    (set! pc (+4 pc))
+					    (next-insn))
+    _ _ -> (error "insn-plus")
+    ))
 
-(define (insn-minus)
-  (set! regs[vm-data[pcd]] (- regs[vm-data[(+ pcd 1)]] regs[vm-data[(+ pcd 2)]]))
-  (set! pcd (+ pcd 3))
-  ((next-insn)))
+(define (insn-sub)
+  (printn "sub")
+  (match regs[CODE[(+2 pc)]] regs[CODE[(+3 pc)]] with
+    (object:int x) (object:int y) -> (begin (set! regs[CODE[(+1 pc)]] (object:int (- x y)))
+					    (set! pc (+4 pc))
+					    (next-insn))
+    _ _ -> (error "insn-minus")
+    ))
 
-(define (insn-equals)
-  (set! regs[vm-data[pcd]] (= regs[vm-data[(+ pcd 1)]] regs[vm-data[(+ pcd 2)]]))
-  (set! pcd (+ pcd 3))
-  ((next-insn)))
+(define (insn-eq)
+  (printn "eq")
+  (match regs[CODE[(+2 pc)]] regs[CODE[(+3 pc)]] with
+    (object:int x) (object:int y) -> (begin (set! regs[CODE[(+1 pc)]] (object:bool (= x y)))
+					    (set! pc (+4 pc))
+					    (next-insn))
+    _ _ -> (error "insn-equals")
+    ))
 
-(match (load sys:argv[1]) with
-  (insn-stream:t insns data)
-  -> (begin
-       (set! vm-insns (list->vector insns))
-       (set! vm-data (list->vector data))
-       (printn vm-insns)
-       (printn vm-data)
-       (set! pci 0)
-       (set! pcd 0)
-       (vm-insns[0])
-       )
+(define (insn-tst)
+  ;; tst <reg> <size> <then-code> <jmp> <else-code>
+  (printn "tst")
+  (printn regs)
+  (printn regs[CODE[(+1 pc)]])
+  (match regs[CODE[(+1 pc)]] with
+    (object:bool #t) -> (begin (set! pc (+3 pc)) (next-insn))
+    (object:bool #f) -> (begin (set! pc (+3 (+ pc CODE[(+2 pc)]))) (next-insn))
+    _ -> (error "insn-tst") ;; impossible
+    ))
+
+(define (insn-jmp)
+  (printn "jmp")
+  (match regs[CODE[(+1 pc)]] with
+    (object:int offset) -> (begin (set! pc (+ (+2 pc) offset)) (next-insn))
+    _ -> (error "insn-jmp") ;; impossible
+    ))
+
+;; consider for CODE: a 16-bit or 32-bit untagged integer array?
+;; insn data
+(define CODE #(0))
+;; literals
+(define LITS #((object:int 0)))
+;; opcodes
+(define OPS #(insn-lit insn-ret insn-add insn-sub insn-eq insn-tst insn-jmp))
+
+(let ((code (load sys:argv[1])))
+  (set! CODE (list->vector code.code))
+  (set! LITS (list->vector code.lits))
+  (printn OPS)
+  (printn CODE)
+  (printn LITS)
+  (set! pc 0)
+  (next-insn)
   )
-
-
-
-
-	     
-	     
-	       
-  
