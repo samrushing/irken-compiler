fee409bddb6f6b90ebf54dfb1e913ffab9e10e2e
Sam Rushing <gitsam@rushing.nightmare.com>
2010-04-26 18:23:37 +0000

    moved context object into its own module start the shift toward passing optional args via context "-nrc" -DNO_RANGE_CHECK option switch back to /usr/local/bin/gcc (probably until dragonegg works)

--- compile.py
+++ compile.py
@@ -9,53 +9,25 @@ import analyze
 import cps
 import backend
 import os
+import context
 
-class context:
-    # maintain some context between passes
-    def __init__ (self):
-        self.datatypes = {}
-        self.dep_graph = None
-        self.scc_graph = None
-        self.scc_map = None
-        self.var_dict = None
-        self.record_types = None
-        self.datatypes = {}
-        self.cincludes = set()
-        self.records2 = {}
-        self.labels2 = {}
-        self.variant_labels = {
-            # Hack.  since pxll.h and header.c:dump_object() already
-            #   know about the 'builtin' cons and nil constructors, we
-            #   hard-code them in here.  You'll note that TC_PAIR is
-            #   two entries before TC_USEROBJ...
-            'cons': -2,
-            'nil': -3,
-            }
-        # don't throw away precious tag space.
-        self.nvariant_offset = len(self.variant_labels)
-        # constructed literals
-        self.constructed = []
-        self.symbols = {}
-
-def compile_file (f, name, annotate=True, noinline=False, verbose=False, trace=False, step_solver=False, typetype=False):
+def compile_file (f, name, c):
     base, ext = os.path.splitext (name)
     print 'read...'
     r = lisp_reader.reader (f)
     exp = r.read_all()
 
-    if verbose:
+    if c.verbose:
         print '--- read ---'
         pp (exp)
 
     # XXX might go easier on memory if we discarded the results of
     #  each pass after using it... 8^)
 
-    c = context()
-
     print 'transform...'
     t = transform.transformer (c)
     exp2 = t.go (exp)
-    if verbose:
+    if c.verbose:
         print '--- transform ---'
         pp (exp2)
 
@@ -72,57 +44,58 @@ def compile_file (f, name, annotate=True, noinline=False, verbose=False, trace=F
 
     print 'typing...'
     # run the constraint generator and solver to find types
-    t = solver.typer (c, verbose, step=step_solver)
+    t = solver.typer (c, c.verbose, step=c.step_solver)
 
-    if typetype:
+    if c.typetype:
         t.go (exp3)
 
-    if verbose:
+    if c.verbose:
         print '--- typing 1 ---'
         exp3.pprint()
 
-    a = analyze.analyzer (c, noinline, verbose)
+    a = analyze.analyzer (c)
     exp4 = a.analyze (exp3)
 
-    if verbose:
+    if c.verbose:
         print '--- analyzer ---'
         exp4.pprint()
 
-    t2 = solver.typer (c, verbose, step=step_solver)
+    t2 = solver.typer (c, c.verbose, step=c.step_solver)
     t.go (exp4)
     #import cProfile
     #cProfile.runctx ("t.go (exp4)", globals(), locals())
-    if verbose:
+    if c.verbose:
         print '--- typing 2 ---'
         exp4.pprint()
 
-    ic = cps.cps (c, verbose=verbose)
+    ic = cps.cps (c, verbose=c.verbose)
     exp5 = ic.go (exp4)
 
-    if verbose:
+    if c.verbose:
         print '--- cps ---'
         cps.pretty_print (exp5)
 
     fo = open ('%s.c' % base, 'wb')
     num_regs = cps.the_register_allocator.max_reg
-    b = backend.c_backend (fo, name, num_regs, c, annotate=annotate, trace=trace)
+    b = backend.c_backend (fo, name, num_regs, c)
     b.emit (exp5)
     b.done()
     fo.close()
+    cc (name, c)
 
-def cc (name, force_32=False, optimize=False, cc='gcc'):
+def cc (name, context):
     import os
     base, ext = os.path.splitext (name)
     uname = os.uname()
     machine = uname[-1]
     if machine == 'amd64':
-        if force_32:
+        if context.force_32:
             arch = '-m32'
         else:
             arch = '-m64'
     else:
         arch = ''
-    if optimize:
+    if context.optimize:
         #optimize = '-O3'
         optimize = '-O2'
     else:
@@ -131,10 +104,13 @@ def cc (name, force_32=False, optimize=False, cc='gcc'):
         # doesn't work as of os x 10.6 [and won't work until clang supports lexical funs]
         #cc = '/Developer/usr/bin/clang'
         # *does* work as of os x 10.6
-        cc = '/Developer/usr/bin/llvm-gcc'
-        arch += ' -fnested-functions'
-        if not force_32:
+        #cc = '/Developer/usr/bin/llvm-gcc'
+        cc = '/usr/local/bin/gcc'
+        #arch += ' -fnested-functions'
+        if not context.force_32:
             arch += ' -m64'
+    if context.no_range:
+        arch += ' -DNO_RANGE_CHECK '
     cmd = '%s -I. -g %s %s %s.c -o %s' % (cc, arch, optimize, base, base)
     print cmd
     os.system (cmd)
@@ -157,18 +133,20 @@ if __name__ == '__main__':
         else:
             return False
 
-    optimize = argtest ('-O')
-    annotate = argtest ('-a')
-    verbose  = argtest ('-v')
-    trace    = argtest ('-t')
-    noinline = argtest ('-ni')
-    force_32 = argtest ('-f32')
-    step_solver = argtest ('-ss')
+    c = context.context()
+
+    c.optimize = argtest ('-O')
+    c.annotate = argtest ('-a')
+    c.verbose  = argtest ('-v')
+    c.trace    = argtest ('-t')
+    c.noinline = argtest ('-ni')
+    c.force_32 = argtest ('-f32')
+    c.step_solver = argtest ('-ss')
     # run the type solver *before* inlining as well as after.
-    typetype = argtest ('-tt')
+    c.typetype = argtest ('-tt')
+    c.no_range = argtest ('-nrc')
 
     if '-f' in sys.argv:
         sys.argv.remove ('-f')
         name = sys.argv[1]
-        compile_file (open (name, 'rb'), name, annotate, noinline, verbose, trace, step_solver, typetype)
-        cc (name, optimize=optimize)
+        compile_file (open (name, 'rb'), name, c)
