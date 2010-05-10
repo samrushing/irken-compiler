8327826999ed1014fe92a5076be8dae9730fa69b
Sam Rushing <gitsam@rushing.nightmare.com>
2010-05-10 19:54:20 +0000

    use zero?

--- tests/tak20.scm
+++ tests/tak20.scm
@@ -5,8 +5,8 @@
 (define (- a b)
   (%%cexp (int int -> int) "%s-%s" a b))
 
-(define (= a b)
-  (%%cexp (int int -> bool) "%s==%s" a b))
+(define (zero? a)
+  (%%cexp (int -> bool) "%s==0" a))
 
 (define (tak x y z)
   (if (>= y x)
@@ -17,6 +17,6 @@
 
 (let loop ((n 20))
   (let ((r (tak 18 12 6)))
-    (if (= n 0)
+    (if (zero? n)
 	r
 	(loop (- n 1)))))
