fd6b1f8ac4542ae9e49ec9f408794c006a4e597b
Sam Rushing <gitsam@rushing.nightmare.com>
2010-05-10 19:55:06 +0000

    rewritten to use pattern matching

--- lib/frb.scm
+++ lib/frb.scm
@@ -7,8 +7,7 @@
 ;; where the color is stored as a bool in each node.
 ;;
 ;; Instead, we save space by encoding the color into the header of
-;;   each node, which leads to some minor code duplication (due to the
-;;   lack of real pattern matching).
+;;   each node.
 
 (datatype tree
   (:red    (tree 'a 'b) (tree 'a 'b) 'a 'b)
@@ -18,101 +17,75 @@
 
 (define (tree:insert root < k v)
 
-  (define (lbalance l r k v)
-    (vcase tree l
-      ((:red ll lr lk lv)
-       (vcase tree ll
-	 ((:red lll llr llk llv)
-	  (tree:red (tree:purple lll llr llk llv) (tree:purple lr r k v) lk lv))
-	 ((:purple _ _ _ _)
-	  (vcase tree lr
-	    ((:red lrl lrr lrk lrv)
-	     (tree:red (tree:purple ll lrl lk lv) (tree:purple lrr r k v) lrk lrv))
-	    ((:purple _ _ _ _)
-	     (tree:purple l r k v))
-	    ((:empty)
-	     (tree:purple l r k v))))
-	 ((:empty)
-	  (tree:purple l r k v))))
-      ((:purple _ _ _ _)
-       (tree:purple l r k v))
-      ((:empty)
-       (tree:purple l r k v))))
+  ;; you can't have a red node directly underneath another red node.
+  ;; these two functions detect that condition and adjust the tree to
+  ;; maintain that invariant.
+
+  (define lbalance
+    (tree:red (tree:red A B k0 v0) C k1 v1) D k2 v2 -> (tree:red (tree:purple A B k0 v0) (tree:purple C D k2 v2) k1 v1)
+    (tree:red A (tree:red B C k1 v1) k0 v0) D k2 v2 -> (tree:red (tree:purple A B k0 v0) (tree:purple C D k2 v2) k1 v1)
+                                            A B k v -> (tree:purple A B k v))
   
-  (define (rbalance l r k v)
-    (vcase tree r
-      ((:red rl rr rk rv)
-       (vcase tree rr
-	 ((:red rrl rrr rrk rrv)
-	  (tree:red (tree:purple l rl k v) (tree:purple rrl rrr rrk rrv) rk rv))
-	 ((:purple _ _ _ _)
-	  (vcase tree rl
-	    ((:red rll rlr rlk rlv)
-	     (tree:red (tree:purple l rll k v) (tree:purple rlr rr rk rv) rlk rlv))
-	    ((:purple _ _ _ _)
-	     (tree:purple l r k v))
-	    ((:empty)
-	     (tree:purple l r k v))))
-	 ((:empty)
-	  (tree:purple l r k v))))
-      ((:purple _ _ _ _)
-       (tree:purple l r k v))
-      ((:empty)
-       (tree:purple l r k v))))
+  (define rbalance
+    A (tree:red (tree:red B C k1 v1) D k2 v2) k0 v0 -> (tree:red (tree:purple A B k0 v0) (tree:purple C D k2 v2) k1 v1)
+    A (tree:red B (tree:red C D k2 v2) k1 v1) k0 v0 -> (tree:red (tree:purple A B k0 v0) (tree:purple C D k2 v2) k1 v1)
+                                            A B k v -> (tree:purple A B k v))
 
   (define (ins n)
-    (vcase tree n
-      ((:empty)
-       (tree:red (tree:empty) (tree:empty) k v))
-      ((:red l r k2 v2)
-       (cond ((< k k2)
-	      (tree:red (ins l) r k2 v2))
-	     ((< k2 k)
-	      (tree:red l (ins r) k2 v2))
-	     (else n)))
-      ((:purple l r k2 v2)
-       (cond ((< k k2)
+    (match n with
+       (tree:empty)
+       -> (tree:red (tree:empty) (tree:empty) k v)
+
+       (tree:red l r k2 v2)
+       -> (cond ((< k k2)
+		 (tree:red (ins l) r k2 v2))
+		((< k2 k)
+		 (tree:red l (ins r) k2 v2))
+		(else n))
+       
+       (tree:purple l r k2 v2)
+       -> (cond ((< k k2)
 	      (lbalance (ins l) r k2 v2))
 	     ((< k2 k)
 	      (rbalance l (ins r) k2 v2))
-	     (else n)))))
+	     (else n))))
 
   (let ((s (ins root)))
-    (vcase tree s
-      ((:purple _ _ _ _) s)
-      ((:red l r k v) (tree:purple l r k v))
-      ((:empty) s) ;; impossible, should raise something here?
-      )))
+    (match s with
+      (tree:red l r k0 v0) -> (tree:purple l r k0 v0)
+      _ -> s
+      ))
+
+  )
 
 (define (tree:member root < key)
   (let member0 ((n root))
-    (vcase tree n
-       ((:empty) (maybe:no))
-       ((:red l r k v)
-	(cond ((< key k) (member0 l))
-	      ((< k key) (member0 r))
-	      (else (maybe:yes v))))
-       ((:purple l r k v)
-	(cond ((< key k) (member0 l))
-	      ((< k key) (member0 r))
-	      (else (maybe:yes v)))))))
+    (match n with
+      (tree:empty)
+      -> (maybe:no)
 
-(define (tree:inorder t p)
-  (let inorder0 ((n t))
-    (vcase tree n
-      ((:empty) #f)
-      ((:red l r k v)    (inorder0 l) (p k v) (inorder0 r) #f)
-      ((:purple l r k v) (inorder0 l) (p k v) (inorder0 r) #f)
+      (tree:red l r k v)
+      -> (cond ((< key k) (member0 l))
+	       ((< k key) (member0 r))
+	       (else (maybe:yes v)))
+
+      (tree:purple l r k v)
+      -> (cond ((< key k) (member0 l))
+	       ((< k key) (member0 r))
+	       (else (maybe:yes v)))
       )))
 
-(define (tree:reverse n p)
-  (let reverse0 ((n n))
-    (vcase tree n
-      ((:empty) #f)
-      ((:red l r k v)    (reverse0 r) (p k v) (reverse0 l) #f)
-      ((:purple l r k v) (reverse0 r) (p k v) (reverse0 l) #f)
-      )))
+(define tree:inorder
+  _ (tree:empty) -> #f
+  p (tree:red l r k v)    -> (begin (tree:inorder p l) (p k v) (tree:inorder p r) #f)
+  p (tree:purple l r k v) -> (begin (tree:inorder p l) (p k v) (tree:inorder p r) #f)
+  )
 
+(define tree:reverse
+  _ (tree:empty) -> #f
+  p (tree:red l r k v)    -> (begin (tree:reverse p r) (p k v) (tree:reverse p l) #f)
+  p (tree:purple l r k v) -> (begin (tree:reverse p r) (p k v) (tree:reverse p l) #f)
+  )
 
 ;; the defn of make-generator, call/cc, etc... makes it pretty hard
 ;;  to pass more than one arg through a continuation.  so instead we'll
