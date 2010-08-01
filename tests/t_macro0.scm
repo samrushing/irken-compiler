4876fa2181eabe80db09676b1d2378853506a1ad
Sam Rushing <gitsam@rushing.nightmare.com>
2010-08-01 21:22:11 +0000

    defmacro

---
+++ tests/t_macro0.scm
@@ -0,0 +1,12 @@
+;; -*- Mode: Irken -*-
+
+(define (< a b)
+  (%%cexp (int int -> bool) "%s<%s" a b))
+
+(defmacro zand
+  (zand)                 -> #t
+  (zand test)            -> test
+  (zand test1 test2 ...) -> (if test1 (zand test2 ...) #f)
+  )
+
+(zand (< 1 2) (< 3 4) (< 5 6))
