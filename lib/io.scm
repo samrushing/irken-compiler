544cc7d89c930fedb683fecbe740c044897a03ce
Sam Rushing <gitsam@rushing.nightmare.com>
2010-08-01 21:40:37 +0000

    colon is now a reserved syntax, use class/method instead

--- lib/io.scm
+++ lib/io.scm
@@ -43,57 +43,57 @@
 
 ;; file I/O 'object'
 
-(define (file:open-read path)
+(define (file/open-read path)
   { fd = (open path O_RDONLY 0)
     buf = (make-string 16384)
     pos = 0
     end = 0 })
 
-(define (file:open-write path create? mode)
+(define (file/open-write path create? mode)
   { fd = (open path (if create? (+ O_WRONLY O_CREAT) O_WRONLY) mode)
     buf = (make-string 16384)
     pos = 0 })
 
-(define (file:close self)
+(define (file/close self)
   (close self.fd))
 
-(define (file:fill-buffer self)
+(define (file/fill-buffer self)
   (let ((n (read-into-buffer self.fd self.buf)))
     (set! self.end n)
     (set! self.pos 0)
     n))
 
-(define (file:read-buffer self)
+(define (file/read-buffer self)
   (cond ((< self.pos self.end)
 	 (let ((opos self.pos))
 	   (set! self.pos self.end)
 	   (substring self.buf opos self.end)))
-	((= (file:fill-buffer self) 0) "")
+	((= (file/fill-buffer self) 0) "")
 	(else
 	 (let ((r (substring self.buf self.pos self.end)))
 	   (set! self.end 0)
 	   (set! self.pos 0)
 	   r))))
 
-(define (file:read-char self)
+(define (file/read-char self)
   (cond ((< self.pos self.end)
 	 (set! self.pos (+ self.pos 1))
 	 (string-ref self.buf (- self.pos 1)))
-	((= (file:fill-buffer self) 0) #\eof)
+	((= (file/fill-buffer self) 0) #\eof)
 	(else
-	 (file:read-char self))))
+	 (file/read-char self))))
 
-(define (file:flush self)
+(define (file/flush self)
   (let loop ((start 0))
     (let ((n (write-substring self.fd self.buf start self.pos)))
       (if (< n self.pos)
 	  (loop n)
 	  #u))))
 
-(define (file:write-char self ch)
+(define (file/write-char self ch)
   (cond ((< self.pos (string-length self.buf))
 	 (string-set! self.buf self.pos ch)
 	 (set! self.pos (+ self.pos 1)))
 	(else
-	 (file:flush self)
-	 (file:write-char self ch))))
+	 (file/flush self)
+	 (file/write-char self ch))))
