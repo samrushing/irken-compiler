e02531581150901f59d50e507dda3f0e17e9cbe5
Sam Rushing <gitsam@rushing.nightmare.com>
2010-08-01 21:34:47 +0000

    colon is now a reserved syntax, use class/method instead

--- tests/t21.scm
+++ tests/t21.scm
@@ -8,32 +8,32 @@
 (include "lib/io.scm")
 
 ;; copy file to stdout
-(let ((f (file:open-read "gc.c")))
-  (let loop ((buffer (file:read-buffer f)))
+(let ((f (file/open-read "gc.c")))
+  (let loop ((buffer (file/read-buffer f)))
     (cond ((> (string-length buffer) 0)
 	   (write 1 buffer)
-	   (loop (file:read-buffer f)))))
-  (file:close f))
+	   (loop (file/read-buffer f)))))
+  (file/close f))
 
 ;; read a few characters...
-(let ((f (file:open-read "gc.c")))
+(let ((f (file/open-read "gc.c")))
   (let loop ((n 10))
     (cond ((= n 0) #t)
 	  (else 
-	   (printn (file:read-char f))
+	   (printn (file/read-char f))
 	   (loop (- n 1)))))
-  (file:close f))
+  (file/close f))
 
 ;; write a file by chars...
-(let ((f (file:open-write "thing.txt" #t #o644)))
+(let ((f (file/open-write "thing.txt" #t #o644)))
   ;;(print-string "fd=") (printn f.fd)
-  (file:write-char f #\H)
-  (file:write-char f #\o)
-  (file:write-char f #\w)
-  (file:write-char f #\d)
-  (file:write-char f #\y)
-  (file:write-char f #\newline)
-  (file:flush f)
-  (file:close f)
+  (file/write-char f #\H)
+  (file/write-char f #\o)
+  (file/write-char f #\w)
+  (file/write-char f #\d)
+  (file/write-char f #\y)
+  (file/write-char f #\newline)
+  (file/flush f)
+  (file/close f)
   )
   
