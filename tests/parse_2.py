4a41d43124fdc450d1be4629d4b9e83e82dedca9
Sam Rushing <gitsam@rushing.nightmare.com>
2010-09-17 20:47:05 +0000

    extra goop for the testing

--- tests/parse_2.py
+++ tests/parse_2.py
@@ -5,4 +5,14 @@ def thing (a, b):
     else:
         raise Glerk()
     c = a / b
+    while 1:
+        break
+    if True:
+        return 12
+    elif False:
+        return 13
+    else:
+        return 14
     return c + 12
+
+zinger = lambda x: x
