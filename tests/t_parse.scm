e27da2ebe6a99ec995b89ce4d3424e7e42b7285c
Sam Rushing <gitsam@rushing.nightmare.com>
2010-10-29 03:36:08 +0000

    datatype decls are now in with the generated parser tables

--- tests/t_parse.scm
+++ tests/t_parse.scm
@@ -17,18 +17,6 @@
 
 ;; parser tables
 
-(datatype action
-  (:shift int)
-  (:reduce int int))
-
-(datatype action-list
-  (:nil)
-  (:cons int (action) (action-list)))
-
-(datatype goto-list
-  (:nil)
-  (:cons int int (goto-list)))
-
 (include "parse/t1.scm")
 
 ;; stack = (:elem item state stack) | (:empty)
