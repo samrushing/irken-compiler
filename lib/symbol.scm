9df81b48169619f85920bb3132d0730cdf025614
Sam Rushing <gitsam@rushing.nightmare.com>
2010-12-05 23:00:26 +0000

    symbol->string: no need for %%cexp intern-symbol: should take a symbol, not a string initialize-symbol-table: with all static symbols found by the compiler

--- lib/symbol.scm
+++ lib/symbol.scm
@@ -12,23 +12,43 @@
 (define (string->uninterned-symbol str)
   (symbol:t str))
 
-(define (symbol->string sym)
-  (%%cexp (symbol -> string) "%s[1]" sym))
+(define symbol->string
+  (symbol:t str) -> str
+  )
 
 (define the-symbol-table (tree:empty))
 
-(define (intern-symbol str)
-  (let ((sym (string->uninterned-symbol str)))
-    (set! the-symbol-table
-	  (tree/insert the-symbol-table string<? str sym))
-    sym))
+(define (intern-symbol sym)
+  (set! the-symbol-table
+	(tree/insert the-symbol-table
+		     string<?
+		     (symbol->string sym)
+		     sym))
+  sym
+  )
 
 (define (string->symbol str)
   (let ((probe (tree/member the-symbol-table string<? str)))
     (match probe with
-      (maybe:no) -> (intern-symbol str)
+      (maybe:no) -> (intern-symbol (string->uninterned-symbol str))
       (maybe:yes sym) -> sym
       )))
 
 (define (symbol<? s1 s2)
   (string<? (symbol->string s1) (symbol->string s2)))
+
+(define (initialize-symbol-table)
+  (let ((v (%%cexp (vector symbol) "(object *) pxll_internal_symbols")))
+    (let loop ((i 0))
+      (if (= i (vector-length v))
+	  #u
+	  (begin
+	    (intern-symbol v[i])
+	    (loop (+ i 1)))))))
+
+(initialize-symbol-table)
+
+  
+
+
+	
