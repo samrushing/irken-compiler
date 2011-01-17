9918ccbd31fafe040037db705ba80b6785a7835a
Sam Rushing <gitsam@rushing.nightmare.com>
2011-01-17 00:58:53 +0000

    new test

---
+++ tests/t_sort.scm
@@ -0,0 +1,18 @@
+;; -*- Mode: Irken -*-
+
+(include "lib/core.scm")
+(include "lib/pair.scm")
+(include "lib/random.scm")
+
+(srandom 314159)
+
+(define nrandom
+  acc 0 -> acc
+  acc n -> (nrandom
+	    (list:cons (random) acc)
+	    (- n 1)))
+
+(let ((rl (nrandom '() 100)))
+  (printn rl)
+  (printn (sort < rl)))
+  
