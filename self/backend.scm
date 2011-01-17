69b049a9e8aa670cbcb61198572fe9a1e757e85a
Sam Rushing <gitsam@rushing.nightmare.com>
2011-01-17 00:57:22 +0000

    wrap-in, wrap-out: new cexp-subst: substitute into a cexp template emit-cexp: emit code for cexp insn emit-call: emit code for non-tail function call

--- self/backend.scm
+++ self/backend.scm
@@ -58,21 +58,64 @@
     (lambda ()
       (format "L" (int (counter.inc))))))
 
+(define (wrap-in types args)
+  (define (wrap type arg)
+    (match type with
+      (type:base b) -> (match b with
+			 'int	 -> (format "unbox(" arg ")")
+			 'string -> (format "((pxll_string*)(" arg "))->data")
+			 _	 -> arg)
+      (type:tvar v) -> arg
+      (type:pred name predargs)
+      -> (match name with
+	   'arrow  -> arg
+	   'vector -> arg
+	   'symbol -> arg
+	   'raw    -> (match predargs with
+			((type:base 'string)) -> (format "((pxll_string*)(" arg "))")
+			_ -> (error1 "unknown raw type in %cexp" type))
+	   _ -> (error1 "unexpected predicate in cexp type sig" type))))
+  (map2 wrap types args))
+
+(define (wrap-out type exp)
+  (match type with
+    (type:base 'int)	-> (format "box(" exp ")")
+    (type:pred 'bool _) -> (format "PXLL_TEST(" exp ")")
+    _			-> exp
+    ))
+
+;; substitute <values> into <template>, e.g. "%0 + %1" ("unbox(r3)" "unbox(r5)") => "r3
+(define (cexp-subst template values)
+  (let ((split (string-split template #\%)))
+    (let loop ((r (LIST (car split)))
+	       (l (cdr split)))
+      (match l with
+	;; wouldn't it be cool to generalize pattern matching to strings somehow?
+	()	    -> (string-concat (reverse r))
+	("")	    -> (error1 "malformed cexp template string" template) ;; template should not end with %
+	("" x . tl) -> (loop (prepend x "%" r) tl) ;; %% causes this
+	(x  . tl)   -> (match (alist/lookup dec-map (string-ref x 0)) with
+			  (maybe:no)	-> (error1 "malformed cexp template string" template)
+			  (maybe:yes n) -> (loop (prepend (substring x 1 (string-length x))
+							  (nth values n)
+							  r)
+						 tl))))))
+
 (define emitk o (cont:k _ _ k) -> (emit o k))
 
 (define emit
-  o (insn:return target)	 -> (o.write (format "PXLL_RETURN(" (int target) ");"))
-  o (insn:literal lit k)	 -> (begin (emit-literal o lit (k/target k)) (emitk o k))
-  o (insn:test reg k0 k1 k)	 -> (begin (emit-test o reg k0 k1) (emitk o k))
-  o (insn:jump reg k)		 -> (begin (emit-jump o reg (k/target k)))
-;;  o (insn:cexp template args k)	 -> (begin (emit-cexp o template args (k/target k)) (emitk o k))
-  o (insn:close name body k)	 -> (begin (emit-close o name body k) (emitk o k))
-  o (insn:varref d i k)		 -> (begin (emit-varref o d i (k/target k)) (emitk o k))
-  o (insn:varset d i v k)	 -> (begin (emit-varset o d i v) (emitk o k))
-  o (insn:new-env size k)	 -> (begin (emit-new-env o size (k/target k)) (emitk o k))
-  o (insn:store off arg tup i k) -> (begin (emit-store o off arg tup i) (emitk o k))
-;;  o (insn:invoke fun args k)     -> (begin (emit-call o fun args) (emitk o k))
-  o (insn:tail fun args k)       -> (begin (emit-tail o fun args) (emitk o k))
+  o (insn:return target)	    -> (o.write (format "PXLL_RETURN(" (int target) ");"))
+  o (insn:literal lit k)	    -> (begin (emit-literal o lit (k/target k)) (emitk o k))
+  o (insn:test reg k0 k1 k)	    -> (begin (emit-test o reg k0 k1) (emitk o k))
+  o (insn:jump reg k)		    -> (begin (emit-jump o reg (k/target k)))
+  o (insn:cexp sig template args k) -> (begin (emit-cexp o sig template args (k/target k)) (emitk o k))
+  o (insn:close name body k)	    -> (begin (emit-close o name body k) (emitk o k))
+  o (insn:varref d i k)		    -> (begin (emit-varref o d i (k/target k)) (emitk o k))
+  o (insn:varset d i v k)	    -> (begin (emit-varset o d i v) (emitk o k))
+  o (insn:new-env size k)	    -> (begin (emit-new-env o size (k/target k)) (emitk o k))
+  o (insn:store off arg tup i k)    -> (begin (emit-store o off arg tup i) (emitk o k))
+  o (insn:invoke fun args k)	    -> (begin (emit-call o fun args k) (emitk o k))
+  o (insn:tail fun args k)	    -> (begin (emit-tail o fun args) (emitk o k))
   o x -> (error1 "NYI " x) ;; XXX remove me
   )
 
@@ -103,6 +146,20 @@
 (define (emit-jump o reg target)
   (if (>= reg 0)
       (o.write (format "r" (int target) "=r" (int reg) ";"))))
+
+(define (emit-cexp o sig template args target)
+  (let ((result-type sig)
+	(arg-types '()))
+    (match sig with
+      (type:pred 'arrow (rtype . atypes))
+      -> (begin (set! result-type rtype) (set! arg-types atypes))
+      _ -> #u)
+    (let ((args (map (lambda (reg) (format "r" (int reg))) args))
+	  (args (wrap-in arg-types args))
+	  (exp (wrap-out result-type (cexp-subst template args))))
+      (if (= target -1)
+	  (o.write (format exp ";"))
+	  (o.write (format "r" (int target) " = " exp))))))
 
 (define frob-name (make-name-frobber))
 
@@ -144,6 +201,37 @@
   (let ((goto (format "goto *r" (int fun) ";")))
     (o.write (format "r" (int args) "[1] = r" (int fun) "[2]; lenv = r" (int args) "; " goto))))
 
+(define (emit-call o fun args k)
+  (let ((free (sort < (k/free k))) ;; sorting these might improve things
+	(return-label (label-maker))
+	(nregs (length free))
+	(target (k/target k)))
+    ;; save
+    (o.write (format "t = allocate (TC_SAVE, " (int (+ 3 nregs)) ");"))
+    (let ((saves
+	   (let loop ((i nregs) (l '()))
+	     (if (= i 0)
+		 (string-join l " ")
+		 (loop (- i 1) (list:cons (format "t[" (int (+ i 4)) "] = r" (int (nth free i))) l))))))
+      (o.write (format "t[1] = k; t[2] = lenv; t[3] = &&" return-label "; " saves " k = t;")))
+    ;; call
+    (let ((goto (format "goto *r" (int fun) "[1]")))
+      (if (>= args 0)
+	  (o.write (format "r" (int args) "[1] = r" (int fun) "[2]; lenv = r" (int args) "; " goto ";"))
+	  (o.write (format "lenv = r" (int fun) "[2]; " goto ";"))))
+    ;; label
+    (o.write (format return-label ":"))
+    ;; restore
+    (let ((restores
+	   (let loop ((i nregs) (l '()))
+	     (if (= i 0)
+		 (string-join l " ")
+		 (loop (- i 1) (list:cons (format "r" (int (nth free i)) " = k[" (int (+ i 4)) "];") l))))))
+      (o.write (format restores "; lenv = k[2]; k = k[1];")))
+    (if (>= target 0)
+	(o.write (format "r" (int target) " = result;")))
+    ))
+
 (define (tag n)
   (logior 1 (<< n 1)))
     
@@ -152,10 +240,11 @@
 	(transform (transformer context))
 	;;(exp0 (sexp:list (read-string "42")))
 	;;(exp0 (sexp:list (read-string "(lambda (x y) (set! y 9) (if #t x y))")))
-	;;(exp0 (sexp:list (read-string "((lambda (x y) (%%cexp (int int -> int) \"%s+%s\" x y)) 3 4)")))
-	(exp0 (sexp:list (read-string "((lambda (x y) x) 3 4)")))
+	;;(exp0 (sexp:list (read-string "((lambda (x y) (%%cexp (int int -> int) \"%0+%1\" x y)) 3 4)")))
+	;;(exp0 (sexp:list (read-string "((lambda (x y) x) 3 4)")))
 	;;(exp0 (sexp:list (read-string "(if #t 23 94)")))
-	;;(exp0 (sexp:list (read-string "(%%cexp (int int -> int) \"%s+%s\" 3 4)")))
+	;;(exp0 (sexp:list (read-string "(%%cexp (int int -> int) \"%0+%1\" 3 4)")))
+	(exp0 (sexp:list (read-string "(%%cexp (int int -> int) \"%0+%1\" ((lambda (x y) (%%cexp (int int -> int) \"%0+%1\" x y)) 3 4) 9)")))
 	(exp1 (transform exp0))
 	(node0 (walk exp1))
 	)
