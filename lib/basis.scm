be393f6d63b01eb5ee1f660a7e4063a0826ce676
Sam Rushing <gitsam@rushing.nightmare.com>
2011-02-15 00:53:00 +0000

    kitchen sink

---
+++ lib/basis.scm
@@ -0,0 +1,13 @@
+;; -*- Mode: Irken -*-
+
+(include "lib/core.scm")
+(include "lib/pair.scm")
+(include "lib/string.scm")
+(include "lib/symbol.scm")
+(include "lib/set.scm")
+(include "lib/alist.scm")
+(include "lib/io.scm")
+(include "lib/frb.scm")
+
+
+
