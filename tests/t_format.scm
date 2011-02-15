b2a963d7ea9530fc0385695cb6bc41ea535d7890
Sam Rushing <gitsam@rushing.nightmare.com>
2011-02-15 00:45:34 +0000

    test join keyword

--- tests/t_format.scm
+++ tests/t_format.scm
@@ -6,4 +6,4 @@
 
 (printn (format "Int: " (int 3) " Hex: " (hex 16384) " Bool: " (bool #f) " woo hoo!"))
 (print-string (format-join " " "testing" (int 1) (bool #t) (char #\A) "\n"))
-
+(printn (format "list(" (join id "," '("sexp")) ")" ))
