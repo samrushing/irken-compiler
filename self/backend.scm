096d70c073893c4f36866ec8ecd2f7570dba914f
Sam Rushing <gitsam@rushing.nightmare.com>
2011-02-21 00:11:06 +0000

    testcexp: optimized conditional emit-tail: use label for known functions emit-alloc: two different kinds of tags guess-record-type: new fun emit-primop: new prims test code moved to compile.scm

--- self/backend.scm
+++ self/backend.scm
@@ -3,15 +3,15 @@
 (include "self/cps.scm")
 (include "self/typing.scm")
 (include "self/graph.scm")
+(include "self/analyze.scm")
 
 (define (make-writer file)
   (let ((level 1))
     (define (write-indent)
-      (let loop ((n level))
-	(match n with
-	  0 -> #u
-	  n -> (begin (write file.fd "  ") (loop (- n 1)))
-	  )))
+      (let loop ((i level))
+	(cond ((> i 0)
+	       (write file.fd "  ")
+	       (loop (- i 1))))))
     (define (write-string s)
       (write-indent)
       (write file.fd s)
@@ -106,31 +106,36 @@
 
 (define (emit o insns context)
 
-  (define emitk (cont:k _ _ k) -> (emit k))
+  (define emitk
+    (cont:k _ _ k) -> (emit k)
+    (cont:nil)	   -> #u)
 
-  (define emit
-    (insn:return target)	    -> (o.write (format "PXLL_RETURN(" (int target) ");"))
-    (insn:literal lit k)	    -> (begin (emit-literal lit (k/target k)) (emitk k))
-    (insn:test reg k0 k1 k)	    -> (begin (emit-test reg k0 k1) (emitk k))
-    (insn:jump reg k)		    -> (begin (emit-jump reg (k/target k)))
-    (insn:cexp sig template args k) -> (begin (emit-cexp sig template args (k/target k)) (emitk k))
-    (insn:close name body k)	    -> (begin (emit-close name body k) (emitk k))
-    (insn:varref d i k)		    -> (begin (emit-varref d i (k/target k)) (emitk k))
-    (insn:varset d i v k)	    -> (begin (emit-varset d i v) (emitk k))
-    (insn:new-env size k)	    -> (begin (emit-new-env size (k/target k)) (emitk k))
-    (insn:store off arg tup i k)    -> (begin (emit-store off arg tup i) (emitk k))
-    (insn:invoke fun args k)	    -> (begin (emit-call fun args k) (emitk k))
-    (insn:tail fun args k)	    -> (begin (emit-tail fun args) (emitk k))
-    (insn:push r k)                 -> (begin (emit-push r) (emitk k))
-    (insn:pop r k)                  -> (begin (emit-pop r (k/target k)) (emitk k))
-    (insn:primop name parm args k)  -> (begin (emit-primop name parm args (k/target k)) (emitk k))
-    (insn:move dst var k)           -> (begin (emit-move dst var (k/target k)) (emitk k))
-    (insn:fatbar lab k0 k1 k)       -> (begin (emit-fatbar lab k0 k1) (emitk k))
-    (insn:fail label npop k)        -> (begin (emit-fail label npop) (emitk k))
-    (insn:nvcase test dt tags alts ealt k)
-    -> (begin (emit-nvcase test dt tags alts ealt) (emitk k))
-    x -> (error1 "NYI " x) ;; XXX remove me
-    )
+  (define (emit insn)
+    (emitk
+     (match insn with
+       (insn:return target)		      -> (begin (o.write (format "PXLL_RETURN(" (int target) ");")) (cont:nil))
+       (insn:literal lit k)		      -> (begin (emit-literal lit (k/target k)) k)
+       (insn:test reg k0 k1 k)		      -> (begin (emit-test reg k0 k1) k)
+       (insn:testcexp regs sig tmpl k0 k1 k)  -> (begin (emit-testcexp regs sig tmpl k0 k1) k)
+       (insn:jump reg target)		      -> (begin (emit-jump reg target) (cont:nil))
+       (insn:cexp sig template args k)	      -> (begin (emit-cexp sig template args (k/target k)) k)
+       (insn:close name body k)		      -> (begin (emit-close name body (k/target k)) k)
+       (insn:varref d i k)		      -> (begin (emit-varref d i (k/target k)) k)
+       (insn:varset d i v k)		      -> (begin (emit-varset d i v) k)
+       (insn:new-env size k)		      -> (begin (emit-new-env size (k/target k)) k)
+       (insn:alloc tag size k)		      -> (begin (emit-alloc tag size (k/target k)) k)
+       (insn:store off arg tup i k)	      -> (begin (emit-store off arg tup i) k)
+       (insn:invoke name fun args k)	      -> (begin (emit-call name fun args k) k)
+       (insn:tail name fun args)	      -> (begin (emit-tail name fun args) (cont:nil))
+       (insn:trcall d n args)		      -> (begin (emit-trcall d n args) (cont:nil))
+       (insn:push r k)			      -> (begin (emit-push r) k)
+       (insn:pop r k)			      -> (begin (emit-pop r (k/target k)) k)
+       (insn:primop name parm args k)	      -> (begin (emit-primop name parm args (k/target k)) k)
+       (insn:move dst var k)		      -> (begin (emit-move dst var (k/target k)) k)
+       (insn:fatbar lab k0 k1 k)	      -> (begin (emit-fatbar lab k0 k1) k)
+       (insn:fail label npop)		      -> (begin (emit-fail label npop) (cont:nil))
+       (insn:nvcase test dt tags alts ealt k) -> (begin (emit-nvcase test dt tags alts ealt) k)
+       )))
 
   (define (emit-literal lit target)
     (let ((val
@@ -140,10 +145,11 @@
 	     (literal:char ch) -> (logior 2 (<< (char->ascii ch) 8))
 	     (literal:undef)   -> #x0e
 	     _ -> (error1 "NYI " lit))))
-      (if (= target -1)
-	  (o.write "// ")) ;; why bother with a dead literal?
-      (o.write (format "r" (int target) " = (object *) " (int val) ";"))
-      ))
+      (let ((prefix (if (= target -1)
+			"// dead " ;; why bother with a dead literal?
+			(format "r" (int target)))))
+	(o.write (format prefix " = (object *) " (int val) ";"))
+      )))
 
   (define (emit-test reg k0 k1)
     (o.write (format "if PXLL_IS_TRUE(r" (int reg)") {"))
@@ -156,40 +162,57 @@
     (o.dedent)
     (o.write "}"))
 
+  (define (emit-testcexp args sig template k0 k1)
+    ;; we know we're testing a cexp, just inline it here
+    (match sig with
+      (type:pred 'arrow (result-type . arg-types) _)
+      -> (let ((args (map (lambda (reg) (format "r" (int reg))) args))
+	       (args (wrap-in arg-types args))
+	       (exp (wrap-out result-type (cexp-subst template args))))
+	   (o.write (format "if PXLL_IS_TRUE(" exp ") {"))
+	   (o.indent)
+	   (emit k0)
+	   (o.dedent)
+	   (o.write "} else {")
+	   (o.indent)
+	   (emit k1)
+	   (o.dedent)
+	   (o.write "}"))
+      _ -> (impossible)))
+
   (define (emit-jump reg target)
     (if (>= reg 0)
 	(o.write (format "r" (int target) "=r" (int reg) ";"))))
 
   (define (emit-cexp sig template args target)
-    (let ((result-type sig)
-	  (arg-types '()))
-      (match sig with
-	(type:pred 'arrow (rtype . atypes) _)
-	-> (begin (set! result-type rtype) (set! arg-types atypes))
-	_ -> #u)
-      (let ((args (map (lambda (reg) (format "r" (int reg))) args))
-	    (args (wrap-in arg-types args))
-	    (exp (wrap-out result-type (cexp-subst template args))))
-	(if (= target -1)
-	    (o.write (format exp ";"))
-	    (o.write (format "r" (int target) " = " exp ";"))))))
+    (match sig with
+      (type:pred 'arrow (result-type . arg-types) _)
+      -> (let ((args (map (lambda (reg) (format "r" (int reg))) args))
+	       (args (wrap-in arg-types args))
+	       (exp (wrap-out result-type (cexp-subst template args))))
+	   (if (= target -1)
+	       (o.write (format exp ";"))
+	       (o.write (format "r" (int target) " = " exp ";"))))
+      _ -> (impossible)
+      ))
 
   (define frob-name (make-name-frobber))
 
   (define (gen-function-label sym)
     (format "FUN_" (frob-name (symbol->string sym))))  
 
-  (define (emit-close name body k)
+  (define (emit-close name body target)
     (let ((proc-label (gen-function-label name))
-	  (jump-label (label-maker))
-	  (target (k/target k))
-	  )
+	  (jump-label (label-maker)))
       ;; emit a jump over the function definition
       (o.write (format "// def " (sym name)))
       (o.write (format "goto " jump-label ";"))
       ;; emit the function definition
       (o.write (format proc-label ":"))
+      (print-string (format "emit-close: " (sym name) " allocates? " (bool (vars-get-flag context name VFLAG-ALLOCATES)) "\n"))
       (o.indent)
+      (if (vars-get-flag context name VFLAG-ALLOCATES)
+	  (o.write "check_heap (0);"))
       (emit body)
       (o.dedent)
       (o.write (format jump-label ":"))
@@ -207,14 +230,24 @@
   (define (emit-new-env size target)
     (o.write (format "r" (int target) " = allocate (TC_TUPLE, " (int (+ size 1)) ");")))
 
+  (define (emit-alloc tag size target)
+    (let ((tag-string
+	   (match tag with
+	     (tag:bare v) -> (format (int v))
+	     (tag:uobj v) -> (format "TC_USEROBJ+(" (int v) "<<2)"))))
+      (o.write (format "r" (int target) " = allocate (" tag-string ", " (int size) ");"))))
+
   (define (emit-store off arg tup i)
     (o.write (format "r" (int tup) "[" (int (+ 1 (+ i off))) "] = r" (int arg) ";")))
 
-  (define (emit-tail fun args)
-    (let ((goto (format "goto *r" (int fun) "[1];")))
+  (define (emit-tail name fun args)
+    (let ((goto
+	   (match name with
+	     (maybe:no)	      -> (format "goto *r" (int fun) "[1];")
+	     (maybe:yes name) -> (format "goto " (gen-function-label name) ";"))))
       (o.write (format "r" (int args) "[1] = r" (int fun) "[2]; lenv = r" (int args) "; " goto))))
 
-  (define (emit-call fun args k)
+  (define (emit-call name fun args k)
     (let ((free (sort < (k/free k))) ;; sorting these might improve things
 	  (return-label (label-maker))
 	  (nregs (length free))
@@ -227,7 +260,11 @@
 		 (format "t[" (int (+ i 4)) "] = r" (int (nth free i))))))
 	(o.write (format "t[1] = k; t[2] = lenv; t[3] = &&" return-label "; " (string-join saves "; ") "; k = t;")))
       ;; call
-      (let ((goto (format "goto *r" (int fun) "[1]")))
+      (let ((goto
+	     (match name with
+	       ;; strange - LLVM actually slows down if I jump to a known label.
+	       (maybe:no)	-> (format "goto *r" (int fun) "[1];")
+	       (maybe:yes name) -> (format "goto " (gen-function-label name) ";"))))
 	(if (>= args 0)
 	    (o.write (format "r" (int args) "[1] = r" (int fun) "[2]; lenv = r" (int args) "; " goto ";"))
 	    (o.write (format "lenv = r" (int fun) "[2]; " goto ";"))))
@@ -243,6 +280,19 @@
 	  (o.write (format "r" (int target) " = result;")))
       ))
 
+  (define (emit-trcall depth name regs)
+    (let ((nargs (length regs))
+	  (npop (- depth 1)))
+      (if (= nargs 0)
+	  ;; a zero-arg trcall needs an extra level of pop
+	  (set! npop (+ npop 1)))
+      (if (> npop 0)
+	  (o.write (format "lenv = ((object " (joins (n-of npop "*")) ")lenv)" (joins (n-of npop "[1]")) ";")))
+      (for-range
+	  i nargs
+	  (o.write (format "lenv[" (int (+ 2 i)) "] = r" (int (nth regs i)) ";")))
+      (o.write (format "goto " (gen-function-label name) ";"))))
+
   (define (emit-push args)
     (o.write (format "r" (int args) "[1] = lenv; lenv = r" (int args) ";")))
 
@@ -250,6 +300,30 @@
     (o.write (format "lenv = lenv[1];"))
     (if (>= target 0)
 	(o.write (format "r" (int target) " = r" (int src)))))
+
+  (define (subset? a b)
+    (every? (lambda (x) (member-eq? x b)) a))
+
+  (define (guess-record-type sig)
+    ;; can we disambiguate this record signature?
+    (let ((sig (map (lambda (x) ;; remove sexp wrapping
+		      (match x with
+			(sexp:symbol field) -> field
+			_ -> (impossible))) sig))
+	  (sig (filter (lambda (x) (not (eq? x '...))) sig)))
+      (let ((candidates '()))
+	(for-each
+	 (lambda (x)
+	   (match x with
+	     (:pair sig0 index0)
+	     -> (if (subset? sig sig0)
+		    (PUSH candidates sig0))))
+	 context.records)
+	(if (= 1 (length candidates))
+	    ;; unambiguous - there's only one possible match.
+	    (maybe:yes (nth candidates 0))
+	    ;; this sig is ambiguous given the set of known records.
+	    (maybe:no)))))
 
   (define (emit-primop name parm args target)
     (define (primop-error)
@@ -276,6 +350,22 @@
 		       (sexp:list (_ (sexp:int index))) (reg)
 		       -> (o.write (format "r" (int target) " = UOBJ_GET(r" (int reg) "," (int index) ");"))
 		       _ _ -> (primop-error))
+	'%record-get -> (match parm args with
+			  (sexp:list ((sexp:symbol label) (sexp:list sig))) (rec-reg)
+			  -> (let ((label-code (lookup-label-code label context)))
+			       (match (guess-record-type sig) with
+				 (maybe:yes sig0)
+				 -> (o.write (format "r" (int target) ;; compile-time lookup
+						     " = ((pxll_vector*)r" (int rec-reg)
+						     ")->val[" (int (index-eq label sig0))
+						     "];"))
+				 (maybe:no)
+				 -> (o.write (format "r" (int target) ;; run-time lookup
+						     " = ((pxll_vector*)r" (int rec-reg)
+						     ")->val[lookup_field((GET_TYPECODE(*r" (int rec-reg)
+						     ")-TC_USEROBJ)>>2," (int label-code)
+						     ")];"))))
+			  _ _ -> (primop-error))
 	_ -> (primop-error))))
 
   (define (emit-move var src target)
@@ -298,8 +388,7 @@
 
   (define (emit-fail label npop)
     (if (> npop 0)
-	(o.write (format "lenv = ((object " (joins (n-of npop "*"))
-			 ")lenv)" (joins (n-of npop "[1]")) ";")))
+	(o.write (format "lenv = ((object " (joins (n-of npop "*")) ")lenv)" (joins (n-of npop "[1]")) ";")))
     (o.write (format "goto fatbar_" (int label) ";")))
 
   (define (which-typecode-fun dt) "get_case") ;; XXX
@@ -357,7 +446,7 @@
   (let ((nreg (+ 1 (context.regalloc.get-max))))
     (for-range
 	i nreg
-	(o.write (format "  register object * r" (int i) ";")))
+	(o.write (format "register object * r" (int i) ";")))
     (o.write "void gc_regs_in (int n) {")
     (o.write "  switch (n) {")
     (for-each
@@ -372,101 +461,3 @@
        (o.write (format "  case " (int (+ i 1)) ": r" (int i) " = heap0[" (int (+ i 3)) "];")))
      (reverse (range nreg)))
     (o.write "}}")))
-
-(define (find-base path)
-  (let ((parts (string-split path #\.))
-	(rparts (reverse parts)))
-    (if (not (string=? (first rparts) "scm"))
-	(error1 "find-base" path)
-	(string-join (reverse (cdr rparts)) "."))))
-
-(define (read-template)
-  (let ((ifile (file/open-read "header.c")))
-    (let loop ((buf (file/read-buffer ifile))
-	       (l '()))
-      (cond ((= (string-length buf) 0) (string-concat (reverse l)))
-	    (else (loop (file/read-buffer ifile)
-			(list:cons buf l)))))))
-
-(define sentinel0 "// CONSTRUCTED LITERALS //\n")
-(define sentinel1 "// REGISTER_DECLARATIONS //\n")
-
-(define (get-header-parts)
-  (let ((header (read-template))
-	(pos0 (string-find sentinel0 header))
-	(pos1 (string-find sentinel1 header))
-	)
-    (if (or (= pos0 -1) (= pos1 -1))
-	(error1 "template strings not found in header.c?" (:pair pos0 pos1))
-	(let ((pos0 (+ pos0 (string-length sentinel0)))
-	      (pos1 (+ pos1 (string-length sentinel1)))
-	      (part0 (substring header 0 pos0))
-	      (part1 (substring header pos0 pos1))
-	      (part2 (substring header pos1 (string-length header))))
-	  (:header part0 part1 part2)))))
-
-(define (system cmd)
-  (%%cexp (string -> int) "system (%0)" cmd))
-
-(define (test)
-  (if (< sys.argc 2)
-      (error "Usage: compile <irken-src-file>"))
-  (let ((context (make-context))
-	(transform (transformer context))
-	(path sys.argv[1])
-	(base (find-base path))
-	(opath (string-append base ".c"))
-	(exp0 (sexp:list (read-file path)))
-	(exp1 (transform exp0))
-	(_ (begin (pp 0 exp1) (newline)))
-	(node0 (walk exp1))
-	(node0 (apply-substs node0))
-	(_ (rename-variables node0))
-	(_ (begin (pp-node node0 0) newline))
-	(graph0 (build-dependency-graph node0))
-	(_ (print-graph graph0))
-	(strong (strongly graph0))
-	(_ (printn strong))
-	(_ (set! context.scc-graph strong))
-	(type0 (type-program node0 context))
-	)
-    (print-string "\n-- reader --\n")
-    (unread exp0)
-    (newline)
-    (print-string "\n-- macros --\n")
-    (unread exp1)
-    (newline)
-    (print-string "\n-- node tree --\n")
-    (pp-node node0 0) (newline)
-    (let ((cps (compile node0 context))
-	  (ofile (file/open-write opath #t #o644))
-	  (o (make-writer ofile)))
-      (print-string "\n-- RTL --\n")
-      (print-insn cps 0)
-      (newline)
-      (print-string "\n-- datatypes --\n")
-      (alist/iterate
-       (lambda (name dt)
-	 (print-datatype dt))
-       context.datatypes)
-      (print-string "\n-- C output --\n")
-      (print-string " : ") (print-string opath) (newline)
-      (match (get-header-parts) with
-	(:header part0 part1 part2)
-	-> (begin (printn part0)
-		  (printn part1)
-		  (printn part2)
-		  (o.copy part0)
-		  (o.copy part1)
-		  (emit-registers o context)
-		  (o.copy part2)
-		  (emit o cps context)))
-      (print-string "done.\n")
-      (o.close)
-      (print-string "compiling...\n")
-      (system (format "/usr/local/bin/gcc -I. -g -m64 " opath " -o " base))
-      )
-    )
-  )
-  
-(test)
