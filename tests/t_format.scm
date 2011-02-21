3bcd0cf7e5a2c10997228db27247edcb0f6c1e77
Sam Rushing <gitsam@rushing.nightmare.com>
2011-02-21 00:16:54 +0000

    tests rpad/lpad/cpad

--- tests/t_format.scm
+++ tests/t_format.scm
@@ -7,3 +7,9 @@
 (printn (format "Int: " (int 3) " Hex: " (hex 16384) " Bool: " (bool #f) " woo hoo!"))
 (print-string (format-join " " "testing" (int 1) (bool #t) (char #\A) "\n"))
 (printn (format "list(" (join id "," '("sexp")) ")" ))
+(printn (format (lpad 10 "hi-") (rpad 10 "there")))
+(printn (rpad 10 "xyz"))
+(printn (lpad 10 "xyz"))
+(printn (cpad 10 "xyz"))
+(printn (format "thing"))
+
