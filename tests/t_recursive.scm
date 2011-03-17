4377ba2c462d5ced6d7d2a1dac3e44e4430c8f1d
Sam Rushing <gitsam@rushing.nightmare.com>
2011-03-17 20:20:20 +0000

    don't let it get confused with the real <length> function

--- tests/t_recursive.scm
+++ tests/t_recursive.scm
@@ -3,9 +3,9 @@
 (include "lib/core.scm")
 (include "lib/pair.scm")
 
-(define length
-  acc {next=(maybe:yes x)} -> (length (+ acc 1) x)
-  acc {next=(maybe:no)}    -> acc
+(define zength
+  acc {next=(maybe:yes x)} -> (zength (+ acc 1) x)
+  acc {next=(maybe:no)}    -> acc)
 
-(length 0 {next=(maybe:no)})
+(zength 0 {next=(maybe:no)})
 
