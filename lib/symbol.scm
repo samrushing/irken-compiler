7b319aa35b2707e91ed784306db1670532b59df9
Sam Rushing <gitsam@rushing.nightmare.com>
2011-03-17 20:25:48 +0000

    symbols now have an index attribute symbol->index: get the index of a symbol intern-symbol, string->symbol: use the size of the symbol table for the index symbol-index<?: sort order for symbols based on index

--- lib/symbol.scm
+++ lib/symbol.scm
@@ -1,12 +1,14 @@
 ;; -*- Mode: Irken -*-
 
-(define (string->uninterned-symbol str)
-  (symbol:t str))
-
 (define symbol->string
-  (symbol:t str) -> str
+  (symbol:t str _) -> str
   )
 
+(define symbol->index
+  (symbol:t _ index) -> index
+  )
+
+(define symbol-table-size 0)
 (define the-symbol-table (tree:empty))
 
 (define (intern-symbol sym)
@@ -15,18 +17,22 @@
 		     string<?
 		     (symbol->string sym)
 		     sym))
+  (set! symbol-table-size (+ 1 symbol-table-size))
   sym
   )
 
 (define (string->symbol str)
   (let ((probe (tree/member the-symbol-table string<? str)))
     (match probe with
-      (maybe:no) -> (intern-symbol (string->uninterned-symbol str))
+      (maybe:no) -> (intern-symbol (symbol:t str symbol-table-size))
       (maybe:yes sym) -> sym
       )))
 
 (define (symbol<? s1 s2)
   (string<? (symbol->string s1) (symbol->string s2)))
+
+(define (symbol-index<? s1 s2)
+  (< (symbol->index s1) (symbol->index s2)))
 
 (define (initialize-symbol-table)
   (let ((v (%%cexp (vector symbol) "(object *) pxll_internal_symbols")))
@@ -34,6 +40,8 @@
       (if (= i (vector-length v))
 	  #u
 	  (begin
+	    ;; it's important that these are interned before any other
+	    ;;   symbol, so that the range of 0..n of symbol ids is unique.
 	    (intern-symbol v[i])
 	    (loop (+ i 1)))))))
 
