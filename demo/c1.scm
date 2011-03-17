d44bee284ab2ec163a9601a3ec4cb1240c81f470
Sam Rushing <gitsam@rushing.nightmare.com>
2011-03-17 23:00:48 +0000

    use inet_ntop and inet_pton, seems more portable

--- demo/c1.scm
+++ demo/c1.scm
@@ -4,7 +4,9 @@
 
 (include "lib/basis.scm")
 
+(cinclude "sys/types.h")
 (cinclude "sys/socket.h")
+(cinclude "netinet/in.h")
 (cinclude "arpa/inet.h")
 
 (define PF_INET		(%%cexp int "PF_INET"))
@@ -16,22 +18,24 @@
 	  "socket (%0, %1, %2)"
 	  family type protocol))
 
-(define (ascii2addr af ascii buf)
+(define (inet_pton af ascii buf)
   (%%cexp (int string (buffer (struct sockaddr_in)) -> int)
-	  "ascii2addr (%0, %1, &(%2->sin_addr))"
+	  "inet_pton (%0, %1, &(%2->sin_addr))"
 	  af ascii buf))
 
-(define (addr2ascii af buf)
-  (let ((r (%%cexp (int (buffer (struct sockaddr_in)) -> cstring)
-		   "addr2ascii (%0, &(%1->sin_addr), sizeof(struct in_addr), 0)"
-		   af buf)))
-    (copy-cstring r)))
+(define (inet_ntop af buf)
+  (let ((ascii (make-string 100))
+	(r (%%cexp (int (buffer (struct sockaddr_in)) string int -> int)
+		   "inet_ntop (%0, &(%1->sin_addr), %2, %3)"
+		   af buf ascii (string-length ascii))))
+    ;; should strip this to NUL
+    ascii))
 
 (define (make-in-addr ip port)
   (let ((ss (%callocate (struct sockaddr_in) 1)))
     (%%cexp ((buffer (struct sockaddr_in)) -> undefined) "(%0->sin_family = PF_INET, PXLL_UNDEFINED)" ss)
     (%%cexp ((buffer (struct sockaddr_in)) int -> undefined) "(%0->sin_port = htons(%1), PXLL_UNDEFINED)" ss port)
-    (trysys (ascii2addr AF_INET ip ss))
+    (trysys (inet_pton AF_INET ip ss))
     ss))
 
 (define (bind fd addr)
@@ -59,14 +63,14 @@
 
 (let ((s (socket PF_INET SOCK_STREAM 0))
       (ss (%callocate (struct sockaddr_in) 1))
-      (addrlen (ascii2addr AF_INET "16.1.0.2" ss))
+      (addrlen (inet_pton AF_INET "16.1.0.2" ss))
       (in-addr (make-in-addr "127.0.0.1" 8888))
       )
   (print-string (format "s = " (int s) "\n"))
   (printn addrlen)
   (if (> addrlen 0)
-      (printn (addr2ascii AF_INET ss))
-      (print-string "ascii2addr failed\n"))
+      (printn (inet_ntop AF_INET ss))
+      (print-string "inet_ntop failed\n"))
   (trysys (bind s in-addr))
   (trysys (listen s 5))
   (let ((fd (trysys (accept s))))
