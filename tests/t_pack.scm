bf10358d9e4a5407e59dd32f53b2e6ef1c534cac
Sam Rushing <gitsam@rushing.nightmare.com>
2011-03-17 20:21:56 +0000

    verify stability

--- tests/t_pack.scm
+++ tests/t_pack.scm
@@ -3,4 +3,8 @@
 (include "lib/core.scm")
 (include "lib/pair.scm")
 
-(pack '(1 1 1 2 3 3 3 4 4 2 2 2) =)
+(define (cmp-first a b)
+  (= (first a) (first b)))
+
+;; this verifies that the packing is 'stable'
+(pack '((1 0) (1 1) (1 2) (2 0) (3 0) (3 1) (3 2) (4 0) (4 1) (2 1) (2 2) (2 3)) cmp-first)
