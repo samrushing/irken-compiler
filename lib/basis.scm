d71d8f4dd1fd2b0207139ca4eb1f0a1acc5002bd
Sam Rushing <gitsam@rushing.nightmare.com>
2011-03-23 22:24:17 +0000

    lib/queue.scm

--- lib/basis.scm
+++ lib/basis.scm
@@ -1,9 +1,12 @@
 ;; -*- Mode: Irken -*-
 
+;; the kitchen sink
+
 (include "lib/core.scm")
 (include "lib/pair.scm")
 (include "lib/string.scm")
 (include "lib/symbol.scm")
+(include "lib/queue.scm")
 (include "lib/set.scm")
 (include "lib/alist.scm")
 (include "lib/io.scm")
