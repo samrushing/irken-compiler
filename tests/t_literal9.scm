d9f76fe5befc727abc6377a9e186d9ce5adb48a2
Sam Rushing <gitsam@rushing.nightmare.com>
2013-02-18 17:47:48 -0800

    sample bytecode files for the demo vm

--- tests/t_literal9.scm
+++ tests/t_literal9.scm
@@ -1,9 +1,38 @@
 
-;(include "lib/core.scm")
-;(include "lib/pair.scm")
+(include "lib/core.scm")
+(include "lib/pair.scm")
 
 (define (thing1 a b) a)
 (define (thing2 a b) b)
 
-#(thing1 thing2)
+;(define v0 #(thing1 thing2))
+;(v0[0] 3 4)
 
+(datatype X
+  (:t string int))
+
+(define opcode-info
+  #((X:t "lit" 2)
+    (X:t "ret" 1)
+    (X:t "add" 3)
+    (X:t "sub" 3)
+    (X:t "eq" 3)
+    (X:t "tst" 2)
+    (X:t "jmp" 1)
+    (X:t "fun" 2)
+    (X:t "tail" 2)
+    (X:t "tail0" 1)
+    (X:t "env" 2)
+    (X:t "arg" 3)
+    (X:t "ref" 3)
+    (X:t "mov" 2)
+    (X:t "push" 1)
+    (X:t "trcall" 3)
+    (X:t "ref0" 2)
+    (X:t "call" 3)
+    (X:t "pop" 1)
+    (X:t "ge" 3)
+    (X:t "print" 1)
+    ))
+
+(printn opcode-info)
