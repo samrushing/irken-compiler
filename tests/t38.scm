8f3661965284a410476d70c01febb24062a87ff4
Sam Rushing <rushing@ampulex.local>
2013-02-25 20:01:16 -0800

    use lib/basis.scm

--- tests/t38.scm
+++ tests/t38.scm
@@ -1,8 +1,3 @@
-(include "lib/core.scm")
-(include "lib/frb.scm")
-(include "lib/pair.scm")
-(include "lib/string.scm")
-(include "lib/symbol.scm")
-
+(include "lib/basis.scm")
 
 {x='thingy y='glorb z='drone}
