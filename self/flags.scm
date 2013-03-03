63801ee2e84f5f4429ba09a5a8ae9b5c99d3e1d8
Sam Rushing <gitsam@rushing.nightmare.com>
2013-03-02 17:33:52 -0800

    note about gcc bug with -g

--- self/flags.scm
+++ self/flags.scm
@@ -1,3 +1,6 @@
 
 (define CC "clang")
-(define CFLAGS "-std=c99 -O3 -fomit-frame-pointer -g -I./include")
+(define CFLAGS "-std=c99 -O3 -fomit-frame-pointer -I./include")
+;;; NOTE: if you are tempted to put "-g" in there and your compiler is gcc,
+;;;   you *must* add -fno-var-tracking as well or your compiles will never finish.
+;;;   See: http://gcc.gnu.org/bugzilla/show_bug.cgi?id=56510
--- util/bootstrap.py
+++ util/bootstrap.py
@@ -13,7 +13,10 @@ def getenv_or (name, default):
         return default
 
 gcc = getenv_or ('CC', 'clang')
-cflags = getenv_or ('CFLAGS', '-std=c99 -O3 -fomit-frame-pointer -g -I./include')
+cflags = getenv_or ('CFLAGS', '-std=c99 -O3 -fomit-frame-pointer -I./include')
+# NOTE: if you are tempted to put "-g" in there and your compiler is gcc,
+#   you *must* add -fno-var-tracking as well or your compiles will never finish.
+#   See: http://gcc.gnu.org/bugzilla/show_bug.cgi?id=56510
 
 windows = platform.uname()[0] == 'Windows'
 
