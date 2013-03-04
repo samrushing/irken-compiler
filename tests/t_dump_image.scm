7191d25f5f179bf48a2c250e3e73285cfa7ccf8b
Sam Rushing <gitsam@rushing.nightmare.com>
2013-03-03 17:27:38 -0800

    updated to use typed callcc/throw

--- tests/t_dump_image.scm
+++ tests/t_dump_image.scm
@@ -1,7 +1,7 @@
+;; -*- Mode: Irken -*- 
 
-(include "lib/core.scm")
-(include "lib/pair.scm")
-(include "lib/string.scm")
+(include "lib/basis.scm")
+(include "lib/os.scm")
 
 (define (thing n)
   (printn "howdy!") 5)
@@ -11,14 +11,14 @@
     (print n) (print-string " ")
     (cond ((= n 0) 9)
 	  ((= n 50)
-	   (call/cc (lambda (k) (dump "test.image" k)))
+	   (callcc (lambda (k) (dump "test.image" k)))
 	   (loop (- n 1)))
 	  (else (loop (- n 1))))))
 
 ;; invoke without an argument to dump the image,
 ;; with an argument to load it and run it.
 (if (> sys.argc 1)
-    ((load "test.image") 0)
+    (throw (load "test.image") 0)
     (go))
 
 
