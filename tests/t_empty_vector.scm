6d17dde3831aa0f3b147ada768c57517217e487d
Sam Rushing <sam-github@rushing.nightmare.com>
2015-10-13 16:21:16 -0700

    update primapp syntax.

--- tests/t_empty_vector.scm
+++ tests/t_empty_vector.scm
@@ -8,15 +8,15 @@
   ;; z gives the gc something to do
 
   (let loop ((n 10000))
-    (let ((v (%make-vector 0 0)))
+    (let ((v (%make-vector #f 0 0)))
       (if (= n 0)
 	  v
 	  (loop (- n 1)))))
   
   (let loop ((n 100000))
     (let ((x #(#() #() #() #() #() #(1 2 3))))
-      (set! x[5] (%make-vector 0 3))
-      (set! x[0] (%make-vector 0 4))
+      (set! x[5] (%make-vector #f 0 3))
+      (set! x[0] (%make-vector #f 0 4))
       )
     (if (= n 0)
 	#t
