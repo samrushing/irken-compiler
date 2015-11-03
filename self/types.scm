0922f9f3857dfd2d9e73dc81d7d4fdde58b49e8c
Sam Rushing <gitsam@rushing.nightmare.com>
2015-11-02 16:11:58 -0800

    experiments with typealias.

--- self/types.scm
+++ self/types.scm
@@ -2,9 +2,16 @@
 
 (include "lib/set.scm")
 
+
+;; temporarily disabled while working on typealias
+
+;; (typealias rtype {parent=(maybe type) pending=bool moo=(maybe type)})
+
 (datatype type
   (:tvar int                {parent=(maybe type) pending=bool moo=(maybe type)})
   (:pred symbol (list type) {parent=(maybe type) pending=bool moo=(maybe type)})
+  ;; (:tvar int rtype)
+  ;; (:pred symbol (list type) rtype)
   )
 
 (define (pred name subs)
@@ -23,6 +30,28 @@
     (type:pred name0 _ _)
     -> (eq? name0 name)
     _ -> #f))
+
+;; define a total ordering on all types
+(define type<?
+  (type:tvar n0 _)  (type:tvar n1 _)  -> (< n0 n1)
+  (type:tvar _ _)   (type:pred _ _ _) -> #t
+  (type:pred _ _ _) (type:tvar _ _ )  -> #f
+  (type:pred p0 args0 _) (type:pred p1 args1 _)
+  -> (cond ((symbol<? p0 p1) #t)
+	   ((symbol<? p1 p0) #f)
+	   ((< (length args0) (length args1)) #t)
+	   ((< (length args1) (length args0)) #f)
+	   (else
+	    (let loop ((args0 args0) (args1 args1))
+	      (match args0 args1 with
+		() _ -> #f
+		(t0 . tl0) (t1 . tl1)
+		-> (if (type<? t0 t1)
+		       #t
+		       (loop (cdr args0) (cdr args1)))
+		_ _ -> (error "impossible")
+		)))))
+
 
 ;; singleton base types
 (define int-type	(pred 'int '()))
@@ -70,8 +99,8 @@
   (type:tvar id _)                    -> (format "t" (int id))
   (type:pred 'arrow (rtype atype) _)  -> (format "(" (type-repr atype) "->" (type-repr rtype) ")")
   (type:pred 'arrow (rtype . args) _) -> (format "(" (join type-repr ", " args) ")->" (type-repr rtype))
-  ;;(type:pred 'rproduct (row) _)       -> (format "{" (row-repr row) "}")
-  (type:pred 'rproduct (row) _)       -> (format "{" (type-repr row) "}")
+  (type:pred 'rproduct (row) _)       -> (format "{" (row-repr row) "}")
+  (type:pred 'rsum (row) _)           -> (format "|" (row-repr row) "|")
   (type:pred pred () _)               -> (format (sym pred))
   (type:pred pred args _)             -> (format (sym pred) "(" (join type-repr ", " args) ")")
   )
@@ -102,7 +131,9 @@
     (match pa pb with
       (type:tvar _ ra) (type:pred _ _ _)    -> (set! ra.parent (maybe:yes pb))
       (type:pred _ _ _) (type:tvar _ rb)    -> (set! rb.parent (maybe:yes pa))
-      (type:tvar _ ra) (type:tvar _ rb)     -> (set! rb.parent (maybe:yes pa))
+      (type:tvar na ra) (type:tvar nb rb)   -> (if (< na nb)
+						   (set! rb.parent (maybe:yes pa))
+						   (set! ra.parent (maybe:yes pb)))
       (type:pred _ _ ra) (type:pred _ _ rb) -> (set! rb.parent (maybe:yes pa)))))
 
 (define (parse-type* exp tvars)
@@ -133,8 +164,63 @@
 	)))
 
   (define parse-predicate
+    ;; without alias check
     ((sexp:symbol p) . rest) -> (pred p (map parse rest))
+    ;; ;; with alias check...
+    ;; ((sexp:symbol p) . rest)
+    ;; -> (match (alist/lookup the-context.aliases p) with
+    ;; 	 (maybe:yes alias) -> (apply-alias p alias rest)
+    ;; 	 (maybe:no) -> (pred p (map parse rest)))
     x -> (error1 "malformed predicate" x))
+
+  ;; ;; similarities here with typing.scm/instantiate-type-scheme
+  ;; (define (apply-alias name alias pred-args)
+  ;;   ;; here we have an alias:sexp, which we can parse into a scheme,
+  ;;   ;;  and then substitute pred-args for the tvars in the scheme.
+  ;;   (printf "apply-alias: " (sym name) "\n")
+  ;;   (let ((tvars (alist-maker))
+  ;; 	  (type (parse-type* alias tvars))
+  ;; 	  (tvmap (alist-maker))
+  ;; 	  )
+  ;;     ;; build a map from tvar.id->arg
+  ;;     (for-each2
+  ;;      (lambda (k v)
+  ;; 	 (tvmap::add k (parse-type v)))
+  ;;      (reverse (tvars::values)) pred-args)
+  ;;     (when the-context.options.debugtyping
+  ;; 	    (printf "apply-alias " (repr alias) "\n")
+  ;; 	    (printf "         to " (join repr "," pred-args) "\n")
+  ;; 	    (printf "parsed type " (type-repr type) "\n")
+  ;; 	    (printf "      tvars " (join type-repr " " (tvars::values)) "\n"))
+  ;;     ;; walk the type, replacing each member of tvars
+  ;;     (let ((moovar (gen-tvar))
+  ;; 	    (recursive? #f)
+  ;; 	    (result
+  ;; 	     (let walk ((t type))
+  ;; 	       (match t with
+  ;; 		 (type:pred name0 args _)
+  ;; 		 -> (begin
+  ;; 		      (printf "  pred compare " (sym name0) " = " (sym name) "\n")
+  ;; 		      (if (eq? name name0)
+  ;; 			  ;; a recursive type alias.  moooo.
+  ;; 			  (begin
+  ;; 			    (printf "found recursion in type alias\n")
+  ;; 			    (set! recursive? #t)
+  ;; 			    moovar
+  ;; 			    )
+  ;; 			  (pred name0 (map walk args)))
+  ;; 		      )
+  ;; 		 _ -> (match (tvmap::get t) with
+  ;; 			(maybe:yes val) -> val
+  ;; 			(maybe:no) -> t)))))
+  ;; 	(let ((result0
+  ;; 	       (if recursive?
+  ;; 		   (pred 'moo (LIST moovar result))
+  ;; 		   result)))
+  ;; 	  (when the-context.options.debugtyping
+  ;; 		(printf "result   " (type-repr result0) "\n"))
+  ;; 	  result0))
+  ;;     ))
 
   (define parse-list
     ((sexp:symbol 'quote) (sexp:symbol tvar)) -> (get-tvar tvar)
@@ -150,13 +236,20 @@
     () -> (rdefault (rabs)))
 
   (define (parse t)
+    (printf "parse " (repr t) "\n")
     (match t with
       (sexp:symbol sym)
       -> (match (alist/lookup base-types sym) with
 	   ;; known base type
 	   (maybe:yes t) -> t
-	   ;; allow nullary predicates
-	   (maybe:no) -> (pred sym '()))
+	   ;;(maybe:no) -> (pred sym '()))
+	   (maybe:no) -> (match (alist/lookup the-context.aliases sym) with
+			   ;; textual substitution, nullary predicate leaves
+			   ;;   all tvars untouched (thus forced to be equal in each use).
+			   (maybe:yes (:scheme gens atype))
+			   -> atype ;; (apply-alias sym alias '())
+			   ;; allow nullary predicates
+			   (maybe:no) -> (pred sym '())))
       (sexp:list l) -> (parse-list l)
       (sexp:record fl) -> (rproduct (parse-field-list fl))
       _ -> (error1 "bad type" exp)))
@@ -208,6 +301,45 @@
   (let ((sig (get-record-sig t)))
     (sexp:list (map sexp:symbol sig))))
 
+;; ok, I think this can be done right after parsing the typealias,
+;;  and I think we can put the substitution back into the datatype
+;;  parser...
+
+(define check-for-typealias-recursion
+  name (:scheme gens type)
+  -> (begin
+       (let ((moovar (new-tvar))
+	     (recursive? #f)
+	     (result
+	      (let walk ((t type))
+		(match t with
+		  (type:pred name0 args _)
+		  -> (begin
+		       (printf "  pred compare " (sym name0) " = " (sym name) "\n")
+		       (if (eq? name name0)
+			   ;; a recursive type alias.  moooo.
+			   ;; XXX what about args? assert null?
+			   (begin
+			     (printf "found recursion in type alias\n")
+			     (set! recursive? #t)
+			     moovar
+			     )
+			   (pred name0 (map walk args)))
+		       )
+		  _ -> t))))
+	 (let ((result0
+		(if recursive?
+		    ;;(:scheme (append gens (LIST moovar)) (pred 'moo (LIST moovar result)))
+		    ;; XXX I don't think its right to add moovar to gens, it's not meant to
+		    ;;     stand for any other type than itself. [but: what if the type is
+		    ;;     specialized, in that case there will be several of them?]
+		    (:scheme gens (pred 'moo (LIST moovar result)))
+		    (:scheme gens result))))
+	   (when the-context.options.debugtyping
+		 (printf "result   " (scheme-repr result0) "\n"))
+	   result0
+	   ))
+       ))
 
 ;; (define (test-types)
 ;;   (let ((t0 (parse-type (car (read-string "((list sexp) -> int)"))))
