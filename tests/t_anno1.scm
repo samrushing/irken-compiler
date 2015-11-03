b867071b73550d12a327cb935d9acd3993f6277a
Sam Rushing <gitsam@rushing.nightmare.com>
2015-11-02 16:16:02 -0800

    new syntax.

--- tests/t_anno1.scm
+++ tests/t_anno1.scm
@@ -1,5 +1,20 @@
 
-(define (thing:(int -> int) x)
+;; (define (thing:(int -> int) x)
+;;   3)
+
+(define (printn x)
+  (%%cexp ('a -> undefined) "dump_object (%0, 0); fprintf (stdout, \"\\n\")" x))
+
+(define (thing x) : (int -> int)
+  (printn x)
+  (printn x)
+  (printn x)
+  (printn x)
+  (printn x)
+  (printn x)
+  (printn x)
   3)
 
-(thing 3)
+(printn (thing 3))
+(printn (thing 4))
+(printn (thing 5))
