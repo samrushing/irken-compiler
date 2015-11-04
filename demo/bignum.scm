100bdde73284af27ada2966c27696983f5aa4b6f
Sam Rushing <gitsam@rushing.nightmare.com>
2015-11-04 01:41:36 -0800

    select digit size by checking platform.

--- demo/bignum.scm
+++ demo/bignum.scm
@@ -12,18 +12,50 @@
 ;; NOTE: multiply and divide algorithms require that
 ;;  the base be half the word size.
 
-;; once everything's working...
-(define big/base #x10000000)
-(define big/repr-width 7)
+;; for 64-bit platforms:
+(define big/base #x20000000)
+(define big/repr-width 8)
 ;; largest power of ten that fits in a digit.
-(define big/decimal-base 1000000000)
-(define big/decimal-pad 9)
+(define big/decimal-base 100000000)
+(define big/decimal-pad 8)
+
+(define (go32)
+  (set! big/base #x20000000)
+  (set! big/repr-width 8)
+  (set! big/decimal-base 100000000)
+  (set! big/decimal-pad 8)
+  )
+
+(define (go16)
+  ;; I think it's the division algorithm that is placing
+  ;;  such drastic limits on the base here.  Anything larger
+  ;;  breaks it.  Needs investigation
+  (printf "bignum: using 16-bit limbs.\n")
+  (set! big/base #x1000)
+  (set! big/repr-width 3)
+  (set! big/decimal-base 100)
+  (set! big/decimal-pad 2)
+  )
+
+;; base16, for testing using relatively small numbers.
+(define (go8)
+  (printf "bignum: using hex-digit limbs.\n")
+  (set! big/base #x10)
+  (set! big/repr-width 1)
+  (set! big/decimal-base 100)
+  (set! big/decimal-pad 2)
+  )
+
+(define (check-platform)
+  (match (get-word-size) with
+    8 -> (go32)
+    4 -> (go16)
+    _ -> (raise (:StrangePlatform))
+    ))
+
+(check-platform)
 
 ;; for testing, use hexadecimal digits
-;(define big/base #x10)
-;(define big/repr-width 1)
-;(define big/decimal-base 100)
-;(define big/decimal-pad 2)
 
 (define (digits-repr digs)
   (let loop ((digs digs) (acc '()))
@@ -405,6 +437,7 @@
   )
 
 (define (test0)
+  (go8)
   (assert (string=? (big-repr (big:zero)) "B0"))
   (assert (string=? (big-repr (int->big #x314159)) "B+3.1.4.1.5.9"))
 
@@ -451,16 +484,21 @@
 
   (printf "larger numbers\n")
   (assert (string=? "B+d.e.a.d.b.e.f.0" (big-repr (big-add (int->big #xdeadbeef) (int->big 1)))))
+  (check-platform)
   )
 
 ;; exhaustive testing of add & sub
 
 (define (exhaustive stop)
+  (go16)
   (let ((counter 0))
     (for-range
 	i stop
+	(printf "\ni= " (int i) " : ")
 	(for-range
 	    j stop
+	    (printf (int j) " ")
+	    (flush)
 	    (let ((a (int->big (+ i j)))
 		  (b (big-add (int->big i) (int->big j)))
 		  (c (int->big (+ (- 0 i) j)))
@@ -487,6 +525,7 @@
 	      )))
     (printf (int counter) " tests passed.\n")
     )
+  (check-platform)
   )
 
 (define (big-fact n)
@@ -548,10 +587,33 @@
     (assert (big= rem (big:zero)))
     ))
 
+(define (test3)
+  (let ((x (big-fact 1000))
+	(s "402387260077093773543702433923003985719374864210714632543799910429938512398629020592044208486969404800479988610197196058631666872994808558901323829669944590997424504087073759918823627727188732519779505950995276120874975462497043601418278094646496291056393887437886487337119181045825783647849977012476632889835955735432513185323958463075557409114262417474349347553428646576611667797396668820291207379143853719588249808126867838374559731746136085379534524221586593201928090878297308431392844403281231558611036976801357304216168747609675871348312025478589320767169132448426236131412508780208000261683151027341827977704784635868170164365024153691398281264810213092761244896359928705114964975419909342221566832572080821333186116811553615836546984046708975602900950537616475847728421889679646244945160765353408198901385442487984959953319101723355556602139450399736280750137837615307127761926849034352625200015888535147331611702103968175921510907788019393178114194545257223865541461062892187960223838971476088506276862967146674697562911234082439208160153780889893964518263243671616762179168909779911903754031274622289988005195444414282012187361745992642956581746628302955570299024324153181617210465832036786906117260158783520751516284225540265170483304226143974286933061690897968482590125458327168226458066526769958652682272807075781391858178889652208164348344825993266043367660176999612831860788386150279465955131156552036093988180612138558600301435694527224206344631797460594682573103790084024432438465657245014402821885252470935190620929023136493273497565513958720559654228749774011413346962715422845862377387538230483865688976461927383814900140767310446640259899490222221765904339901886018566526485061799702356193897017860040811889729918311021171229845901641921068884387121855646124960798722908519296819372388642614839657382291123125024186649353143970137428531926649875337218940694281434118520158014123344828015051399694290153483077644569099073152433278288269864602789864321139083506217095002597389863554277196742822248757586765752344220207573630569498825087968928162753848863396909959826280956121450994871701244516461260379029309120889086942028510640182154399457156805941872748998094254742173582401063677404595741785160829230135358081840096996372524230560855903700624271243416909004153690105933983835777939410970027753472000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"))
+    (assert (string=? (big->dec x) s))
+    ))
+
+(define (test4)
+  (let ((i 73)
+	(j 186)
+	(a (int->big (+ i j)))
+	(b (big-add (int->big i) (int->big j)))
+	(c (int->big (+ (- 0 i) j)))
+	(d (big-add (int->big (- 0 i)) (int->big j)))
+	(e (int->big (- i j)))
+	(f (big-sub (int->big i) (int->big j)))
+	(g (int->big (- (- i) j)))
+	(h (big-sub (int->big (- i)) (int->big j)))
+	)
+    (assert (big= a b))
+    (assert (big= c d))
+    (assert (big= e f))
+    (assert (big= g h))))
+
 ;; Note: to use dump/load, you need to link with -no_pie to disable ASLR
 (define (pi-digit-emitter)
   (let ((n 1))
-    (printf (lpad 6 (int 0)) ": ")
+    (printf (lpad 6 (int 0)) ":  ")
     (define (emit dig)
       (when (= 0 (mod n 100))
 	    (callcc (lambda (k) (dump "pi.image" k)))
@@ -565,6 +627,9 @@
       )
     emit
     ))
+
+;; http://damien-guichard.developpez.com/tutoriels/ocaml/?page=page_6
+;; http://www.pi314.net/eng/schrogosper.php
 
 (define (pi)
   (define B1 (int->big 1))
@@ -601,11 +666,10 @@
 	 )
       )
     )
+  (set-verbose-gc #f)
   (g B1 (IB 180) (IB 60) B2)
   )
 
-;; 65536 tests
-;;(exhaustive #x100)
 ;; 16777216 tests
 ;(exhaustive #x1000)
 
@@ -613,12 +677,13 @@
 ;(when (= big/base 16)
 ;      (test0)
 ;      (test1))
+
+;; 65536 tests
+;(exhaustive #x100)
+;(test0)
 ;(test2)
-
-(define (set-verbose-gc b)
-  (%%cexp (bool -> undefined) "verbose_gc = %0" b))
-
-(set-verbose-gc #f)
+;(test3)
+;(test4)
 
 ;; invoke without an argument to start generating,
 ;; and with an argument to load it and run it.
