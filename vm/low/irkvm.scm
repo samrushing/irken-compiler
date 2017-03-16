ab20e8661ad946f6223169c1fe9abf5d4f574013
Sam Rushing <gitsam@rushing.nightmare.com>
2017-03-16 15:22:08 -0700

    experiments with irken closures.

--- vm/low/irkvm.scm
+++ vm/low/irkvm.scm
@@ -3,19 +3,40 @@
 (include "lib/basis.scm")
 (include "lib/os.scm")
 
-;; Why include this tiny, useless wrapper?  Because we need vm.c to have access to
-;;  everything in the Irken runtime.  This is an easy way to do that.
-
 (local-include "vm.c")
 
-(define (read-bytecode-file path)
-  (%%cexp (string -> int) "read_bytecode_file(%0)" path)
+(define (read-bytecode-file path stsize)
+  (%%cexp (string int -> int) "read_bytecode_file (%0, %1)" path symbol-table-size)
+  )
+
+(define (set-closure closure)
+  (%%cexp ('a -> int) "vm_set_closure (%0)" closure))
+
+(define (writex fd s)
+  (printf "write fd=" (int fd) " s= " (string s) "\n")
+  (write fd s))
+
+(define boot-get-object
+  "O_RDONLY"      -> (magic O_RDONLY)
+  "O_RDWR"        -> (magic O_RDWR)
+  "O_CREAT"       -> (magic O_CREAT)
+  "STDOUT_FILENO" -> (magic STDOUT_FILENO)
+  "open"          -> (magic open)
+  "close"         -> (magic close)
+  "write"         -> (magic writex)
+  x               -> (error1 "boot-get-object" x)
   )
 
 (define (go)
-  (if (not (= -1 (read-bytecode-file (zero-terminate sys.argv[1]))))
-      (%%cexp (-> 'a) "vm_go()")
+  (if (not (= -1 (read-bytecode-file 
+                  (zero-terminate sys.argv[1])
+                  symbol-table-size)))
+      (begin
+        (set-closure boot-get-object)
+        (%%cexp (-> 'a) "vm_go()")
+        )
       (printf "failed to read bytecode file\n"))
   )
 
+(printn (boot-get-object "STDOUT_FILENO"))
 (go)
