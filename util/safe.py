66e2c51a5d5dd8431fe6b74f12a51728be9411fd
Sam Rushing <gitsam@rushing.nightmare.com>
2017-03-17 16:24:39 -0700

    only rename if compile was successful.
    and temporarily rename compile to compile0.

--- util/safe.py
+++ util/safe.py
@@ -1,26 +1,29 @@
 # -*- Mode: Python -*-
 
-# keep the last 5 compilers in case of nasty bugs that are
+# keep the last 10 compilers in case of nasty bugs that are
 #   difficult to back out of.
 
 import os
 import re
 
-files = []
-for path in os.listdir ('./self'):
-    m = re.match ('^compile([0-9])?$', path)
-    if m is not None:
-        if m.group(1) is None:
-            num = 0
-        else:
+def rename_binaries():
+    files = []
+    for path in os.listdir ('./self'):
+        m = re.match ('^compile([0-9])$', path)
+        if m is not None:
             num = int (m.group(1))
-        files.append ((num, path))
+            files.append ((num, path))
 
-files.sort()
-files.reverse()
+    files.sort()
+    files.reverse()
 
-for num, path in files:
-    #print 'self/%s' %path, 'self/compile%d' % (num+1)
-    os.rename ('self/%s' %path, 'self/compile%d' % (num+1))    
+    for num, path in files:
+        #print 'self/%s' %path, 'self/compile%d' % (num+1)
+        os.rename ('self/%s' %path, 'self/compile%d' % (num+1))    
 
-os.system ('self/compile1 self/compile.scm')
+os.rename ('self/compile', 'self/compile0')
+if 0 == os.system ('self/compile0 self/compile.scm'):
+    rename_binaries()
+else:
+    os.rename ('self/compile0', 'self/compile')
+
