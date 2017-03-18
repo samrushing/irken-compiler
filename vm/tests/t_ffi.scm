921b0c5cdbd293dda2df6420d769ff4d6af5f161
Sam Rushing <gitsam@rushing.nightmare.com>
2017-03-18 00:24:41 -0700

    shiny new make-ffi macro, much easy.

--- vm/tests/t_ffi.scm
+++ vm/tests/t_ffi.scm
@@ -4,14 +4,30 @@
 
 ;; test ffi from irken-vm
 
-(define write 
-  (let ((p (%%cexp (string -> int) "dlsym" "write\x00")))
-    (lambda (fd s)
-      (%%cexp (int int int string int -> int)
-              "ffi"
-              p 3 fd s (string-length s))
-      )))
+(defmacro make-ffi
+  (make-ffi name rtype () (ftype0 ...))
+  -> (let (($p (%%cexp (string -> int) "dlsym" name)))
+       (lambda ()
+         (%%cexp (int char int ftype0 ...)
+                 "ffi"
+                 $p rtype 0)))
+  (make-ffi name rtype nargs (formal0 ...) (ftype0 ...))
+  -> (let (($p (%%cexp (string -> int) "dlsym" name)))
+       (lambda (formal0 ...)
+         (%%cexp (int char int ftype0 ...)
+                 "ffi"
+                 $p rtype nargs
+                 formal0 ...))))
+
+(define (tmpnam)
+  ((make-ffi "tmpnam\x00" #\s 0 () (-> string))  ))
+
+(define (write fd s)
+  ((make-ffi "write\x00"
+             #\i 3
+             (fd s slen)
+             (int string int -> int))
+   fd s (string-length s)))
 
 (write 2 "yabba dabba dooooooo\n")
-
-  
+(printn (tmpnam))
