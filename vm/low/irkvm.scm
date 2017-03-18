d0367856df4b542a0045e59d074fe4f533b5c6f5
Sam Rushing <gitsam@rushing.nightmare.com>
2017-03-18 00:19:56 -0700

    read-bytecode-file: no symbol table size.
    thinking now it's a mistake to try to share symbol tables.

--- vm/low/irkvm.scm
+++ vm/low/irkvm.scm
@@ -5,17 +5,16 @@
 
 (local-include "vm.c")
 
-(define (read-bytecode-file path stsize)
-  (%%cexp (string int -> int) "read_bytecode_file (%0, %1)" path symbol-table-size)
+(define (read-bytecode-file path)
+  (%%cexp (string -> int) "read_bytecode_file (%0)" path)
   )
 
+;; this is an (admittedly kludgy) way of bootstrapping the vm->irken
+;;  call interface: we provide a single "object-getting closure"...
 (define (set-closure closure)
   (%%cexp ('a -> int) "vm_set_closure (%0)" closure))
 
-(define (writex fd s)
-  (printf "write fd=" (int fd) " s= " (string s) "\n")
-  (write fd s))
-
+;; ... and these are the exposed objects.
 (define boot-get-object
   "O_RDONLY"      -> (magic O_RDONLY)
   "O_RDWR"        -> (magic O_RDWR)
@@ -23,20 +22,17 @@
   "STDOUT_FILENO" -> (magic STDOUT_FILENO)
   "open"          -> (magic open)
   "close"         -> (magic close)
-  "write"         -> (magic writex)
+  "write"         -> (magic write)
   x               -> (error1 "boot-get-object" x)
   )
 
 (define (go)
-  (if (not (= -1 (read-bytecode-file 
-                  (zero-terminate sys.argv[1])
-                  symbol-table-size)))
-      (begin
-        (set-closure boot-get-object)
-        (%%cexp (-> 'a) "vm_go()")
-        )
-      (printf "failed to read bytecode file\n"))
-  )
+  (cond ((not (= -1 (read-bytecode-file (zero-terminate sys.argv[1]))))
+         (set-closure boot-get-object)
+         (%%cexp (-> 'a) "vm_go()"))
+        (else
+         (printf "failed to read bytecode file\n"))
+        ))
 
 (printn (boot-get-object "STDOUT_FILENO"))
 (go)
