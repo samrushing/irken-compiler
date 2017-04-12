e37da029abb9076b033cbf734bbb25b8189f3be5
Sam Rushing <gitsam@rushing.nightmare.com>
2017-04-12 15:23:10 -0700

    use rbuf/wbuf to distinguish read/write files.

--- lib/io.scm
+++ lib/io.scm
@@ -13,31 +13,31 @@
 
 (define (file/open-read path)
   { fd = (open path O_RDONLY 0)
-    buf = (make-string 16384)
+    rbuf = (make-string 16384)
     pos = 0
     end = 0 })
 
 (define (file/open-write path create? mode)
   { fd = (open path (logior O_TRUNC (if create? (logior O_WRONLY O_CREAT) O_WRONLY)) mode)
-    buf = (make-string 16384)
+    wbuf = (make-string 16384)
     pos = 0 })
 
 (define (file/open-stdin)
   { fd = STDIN_FILENO
-    buf = (make-string 16384)
+    rbuf = (make-string 16384)
     pos = 0
     end = 0 })
 
 (define (file/open-stdout)
   { fd = STDOUT_FILENO
-    buf = (make-string 16384)
+    wbuf = (make-string 16384)
     pos = 0 })
 
 (define (file/close self)
   (close self.fd))
 
 (define (file/fill-buffer self)
-  (let ((n (read-into-buffer self.fd self.buf)))
+  (let ((n (read-into-buffer self.fd self.rbuf)))
     (set! self.end n)
     (set! self.pos 0)
     n))
@@ -46,10 +46,10 @@
   (cond ((< self.pos self.end)
 	 (let ((opos self.pos))
 	   (set! self.pos self.end)
-	   (substring self.buf opos self.end)))
+	   (substring self.rbuf opos self.end)))
 	((= (file/fill-buffer self) 0) "")
 	(else
-	 (let ((r (substring self.buf self.pos self.end)))
+	 (let ((r (substring self.rbuf self.pos self.end)))
 	   (set! self.end 0)
 	   (set! self.pos 0)
 	   r))))
@@ -57,7 +57,7 @@
 (define (file/read-char self)
   (cond ((< self.pos self.end)
 	 (set! self.pos (+ self.pos 1))
-	 (string-ref self.buf (- self.pos 1)))
+	 (string-ref self.rbuf (- self.pos 1)))
 	((= (file/fill-buffer self) 0) #\eof)
 	(else
 	 (file/read-char self))))
@@ -71,7 +71,7 @@
 
 (define (file/flush self)
   (let loop ((start 0))
-    (let ((n (write-substring self.fd self.buf start self.pos)))
+    (let ((n (write-substring self.fd self.wbuf start self.pos)))
       (if (< n self.pos)
 	  (loop n)
 	  (set! self.pos 0))
@@ -79,9 +79,9 @@
 
 (define (file/write self s)
   (let ((slen (string-length s))
-        (blen (string-length self.buf)))
+        (blen (string-length self.wbuf)))
     (cond ((< (+ self.pos slen) blen)
-           (buffer-copy s 0 slen self.buf self.pos)
+           (buffer-copy s 0 slen self.wbuf self.pos)
            (set! self.pos (+ self.pos slen)))
           (else
            (file/flush self)
@@ -90,8 +90,8 @@
           )))
 
 (define (file/write-char self ch)
-  (cond ((< self.pos (string-length self.buf))
-	 (string-set! self.buf self.pos ch)
+  (cond ((< self.pos (string-length self.wbuf))
+	 (string-set! self.wbuf self.pos ch)
 	 (set! self.pos (+ self.pos 1)))
 	(else
 	 (file/flush self)
