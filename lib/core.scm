324bddc0c7b12c9aacbaec07b05e08855c65f15c
Sam Rushing <gitsam@rushing.nightmare.com>
2017-05-17 21:21:09 -0700

    update comment.

--- lib/core.scm
+++ lib/core.scm
@@ -384,9 +384,8 @@
 ;;  this might be the original message:
 ;;  http://list.cs.brown.edu/pipermail/plt-scheme/2006-April/012456.html
 
-;; NOTE: I think we're losing type safety across the generator boundary.
-
 ;; this simpler version uses getcc and putcc directly.
+;; note: the type annotation is necessary.
 (define (make-generator producer) : ((((maybe 'a) -> undefined) -> undefined) -> ( -> (maybe 'a)))
   (let ((ready #f)
         ;; holding useless continuations
