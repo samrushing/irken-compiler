b92040c00bd2c15bcef13141fd164164a756383d
Sam Rushing <gitsam@rushing.nightmare.com>
2017-06-02 16:33:48 -0700

    wikipedia link

--- tests/manboy.scm
+++ tests/manboy.scm
@@ -1,3 +1,7 @@
+;; -*- Mode: Irken -*-
+
+;; https://en.wikipedia.org/wiki/Man_or_boy_test
+
 (include "lib/core.scm")
 
 (define (A k x1 x2 x3 x4 x5)
@@ -7,5 +11,5 @@
   (if (<= k 0)
       (+ (x4) (x5))
       (B)))
- 
+
 (A 10 (lambda () 1) (lambda () -1) (lambda () -1) (lambda () 1) (lambda () 0))
