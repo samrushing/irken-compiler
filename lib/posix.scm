f0116a04898c34531539b90e34d5965fdacb5a99
Sam Rushing <gitsam@rushing.nightmare.com>
2017-07-22 18:14:00 -0700

    converted to new FFI.

--- lib/posix.scm
+++ lib/posix.scm
@@ -1,30 +1,26 @@
 ;; -*- Mode: Irken -*-
 
-(cinclude "sys/utsname.h")
+;; This should really go into lib/os.scm or somewhere like that.
+;; *or* we put posix-only stuff here.
+
+(require-ffi 'posix)
 
 (define (uname)
-  (%backend (c llvm)
-    (let ((utsname (%callocate (struct utsname) 1)))
-      (%%cexp ((buffer (struct utsname)) -> int) "uname (%0)" utsname)
-      {sysname=  (copy-cstring (%%cexp ((buffer (struct utsname)) -> cstring) "%0->sysname" utsname))
-       nodename= (copy-cstring (%%cexp ((buffer (struct utsname)) -> cstring) "%0->nodename" utsname))
-       release=  (copy-cstring (%%cexp ((buffer (struct utsname)) -> cstring) "%0->release" utsname))
-       version=  (copy-cstring (%%cexp ((buffer (struct utsname)) -> cstring) "%0->version" utsname))
-       machine=  (copy-cstring (%%cexp ((buffer (struct utsname)) -> cstring) "%0->machine" utsname))
-       }))
-  (%backend bytecode
-    (let ((utsname (%%cexp (-> (list string)) "plat")))
-      (match utsname with
-        (sysname nodename release version machine)
-        -> {sysname=sysname
-            nodename=nodename
-            release=release
-            version=version
-            machine=machine}
-        _ 
-        -> (raise (:UnameFailed utsname))
-        )))
-  )
-      
 
-       
+  ;; probably useful outside this fun.
+  (define (get-string s*)
+    (let ((s0* (%c-aref char s* 0))
+          (slen (posix/strlen s0*)))
+      (%c-sfromc #f s0* slen)))
+
+  (let ((utsname* (malloc (struct utsname)))
+        (_ (syscall (posix/uname utsname*)))
+        (result
+         {sysname  = (get-string (%c-sref utsname.sysname utsname*))
+          nodename = (get-string (%c-sref utsname.nodename utsname*))
+          release  = (get-string (%c-sref utsname.release utsname*))
+          version  = (get-string (%c-sref utsname.version utsname*))
+          machine  = (get-string (%c-sref utsname.machine utsname*))}))
+    (free utsname*)
+    result
+    ))
