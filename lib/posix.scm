76a6e36c5569f47751a4c828353500494af3d4e3
Sam Rushing <gitsam@rushing.nightmare.com>
2017-07-24 16:49:25 -0700

    use get-cstring from ctype.scm.

--- lib/posix.scm
+++ lib/posix.scm
@@ -6,21 +6,14 @@
 (require-ffi 'posix)
 
 (define (uname)
-
-  ;; probably useful outside this fun.
-  (define (get-string s*)
-    (let ((s0* (%c-aref char s* 0))
-          (slen (posix/strlen s0*)))
-      (%c-sfromc #f s0* slen)))
-
   (let ((utsname* (malloc (struct utsname)))
         (_ (syscall (posix/uname utsname*)))
         (result
-         {sysname  = (get-string (%c-sref utsname.sysname utsname*))
-          nodename = (get-string (%c-sref utsname.nodename utsname*))
-          release  = (get-string (%c-sref utsname.release utsname*))
-          version  = (get-string (%c-sref utsname.version utsname*))
-          machine  = (get-string (%c-sref utsname.machine utsname*))}))
+         {sysname  = (get-cstring (%c-sref utsname.sysname utsname*))
+          nodename = (get-cstring (%c-sref utsname.nodename utsname*))
+          release  = (get-cstring (%c-sref utsname.release utsname*))
+          version  = (get-cstring (%c-sref utsname.version utsname*))
+          machine  = (get-cstring (%c-sref utsname.machine utsname*))}))
     (free utsname*)
     result
     ))
