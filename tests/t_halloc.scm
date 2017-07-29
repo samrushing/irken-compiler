6f749901a496ce4cacb00ac972bc65df1bca2b5e
Sam Rushing <gitsam@rushing.nightmare.com>
2017-07-28 21:19:02 -0700

    test %halloc.
    Note: this does a giant string-consing loop in order
      to verify that the TC_FOREIGN object is correctly
      handled over a gc boundary.

---
+++ tests/t_halloc.scm
@@ -0,0 +1,18 @@
+;; -*- Mode: Irken -*-
+
+(include "lib/basis.scm")
+(include "lib/map.scm")
+
+(let ((buf (%halloc int 20)))
+  (for-range j 1000000
+    (let ((thing (make-string 1024)))
+      (for-range i 20
+        (let ((int* (%c-aref int buf i)))
+          ;; (printn int*)
+          (%c-set-int int (+ i 1234) int*)))))
+  (for-range i 20
+    (let ((int* (%c-aref int buf i)))
+      (printf (lpad 3 (int i)) " " (lpad 8 (int (%c-get-int int int*))) "\n")
+      ))
+  )
+
