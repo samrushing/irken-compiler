6824887aa47f87b03b0b08279a64e1b588583176
Sam Rushing <gitsam@rushing.nightmare.com>
2017-09-21 13:11:28 -0700

    emit autoffi code once per interface.
    add a new context slot `ffi-map` to track which interfaces are
     needed for this compilation.
    bytecode: use new `fetch-ffi-constant` to set constants at runtime.
    also: updated/cleaned comments.

--- self/autoffi.scm
+++ self/autoffi.scm
@@ -1,20 +1,16 @@
 ;; -*- Mode: Irken -*-
 
-;; go through all the signatures and constants from all the interfaces,
-;;  and emit code for them.  hopefully we can use macros to let the compiler
-;;  turn these into The Right Thing for each backend.
-
-;; sig -> c-backend:
-;; (sig srand (int -> void))
-;; => (define (srand arg0) (%ffi2 srand arg0))
+;; AutoFFI - when the compiler comes across a 'require-ffi' form, it needs
+;;   to emit code that will Do the Right Thing depending on which backend
+;;   is being used.  We do this by inserting macro calls with enough information
+;;   to create the needed code for all backends.  The macros (and their translations)
+;;   are defined in lib/ctype.scm.
 ;;
-;; would be nice to have names for the args.  consider
-;;   changing the spec to permit this.
+;; currently, the following macros are used:
 ;;
-;; con -> c-backend:
-;; (con S2N_BLOCKED_ON_READ 1)
-;; => (define S2N_BLOCKED_ON_READ 1)
-;;
+;; fetch-ffi-constant: FFI constants like O_TRUNC
+;; build-ffi-fun: FFI functions like open()
+;; build-ffi-ob: FFI objects like STDIN_FILENO
 
 (define ctype->sexp
   (ctype:name name)    -> (sexp:symbol name)
@@ -47,8 +43,14 @@
   x -> (error1 "ctype->rtype-code: bad rtype" (ctype-repr x))
   )
 
+;; There is no need to distinguish between an ffi interface that has been loaded
+;;   at runtime vs compile-time: they are considered platform-agnostic.  But there *is*
+;;   a need to distinguish between those loaded by the compiler and those loaded by the
+;;   program being compiled.  Hence, the `ffi-map` slot in the global context object.
+
 (define (autoffi interface)
   (let ((info (require-ffi* interface))
+        (first-time? (not (cmap/present? the-context.ffi-map interface)))
         (forms '()))
 
     (define (iface-prefix name)
@@ -58,57 +60,77 @@
       (map (lambda (path) (sexp (sexp:symbol 'cinclude) (sexp:string path)))
            info.includes))
 
-    (set! the-context.ffi-count (+ 1 the-context.ffi-count))
+    (when first-time?
+      ;; only emit forms the first time this interface is loaded
+      (cmap/add the-context.ffi-map interface)
 
-    (match the-context.options.backend with
-      (backend:bytecode)
-      -> (PUSH forms (sexp (sexp:symbol 'require-ffi) (sexp (sexp:symbol 'quote) (sexp:symbol interface))))
-      (backend:c)
-      -> (set! forms (append (iface-includes) forms))
-      (backend:llvm)
-      -> (set! forms (append (iface-includes) forms))
+      (match the-context.options.backend with
+        ;; bytecode needs a runtime call to ``require-ffi``
+        (backend:bytecode)
+        -> (PUSH forms (sexp (sexp:symbol 'require-ffi) (sexp (sexp:symbol 'quote) (sexp:symbol interface))))
+        (backend:c)
+        -> (set! forms (append (iface-includes) forms))
+        (backend:llvm)
+        -> (set! forms (append (iface-includes) forms))
+        )
+
+      ;; constant definitions
+      (info.cons::iterate
+       (lambda (k v)
+         (PUSH forms
+               ;; XXX consider using a zero placeholder for all constants when emitting
+               ;;  bytecode, to avoid needless differences in bytecode compiled on different
+               ;;  platforms.
+               (sexp (sexp:symbol 'define) (sexp:symbol k) (sexp:int v)))))
+
+      (match the-context.options.backend with
+        ;; with the bytecode backend, we need to set! the values after loading the ffi info,
+        ;;  these expressions will hopefully appear after that happens.
+        (backend:bytecode)
+        -> (info.cons::iterate
+            (lambda (k v)
+              (PUSH forms
+                    (sexp (sexp:symbol 'set!)
+                          (sexp:symbol k)
+                          (sexp (sexp:symbol 'fetch-ffi-constant) (sexp:symbol k) (sexp:int v))))))
+        _ -> #u
+        )
+
+      ;; function/variable definitions
+      (info.sigs::iterate
+       (lambda (k v)
+         (match v with
+           (csig:fun name rtype argtypes)
+           -> (let ((ztname (zero-terminate (symbol->string name))))
+                (PUSH forms
+                      (sexp (sexp:symbol 'define)
+                            (sexp:symbol (iface-prefix name))
+                            (sexp (sexp:symbol 'build-ffi-fun)
+                                  (sexp:symbol name)
+                                  (sexp:string ztname)
+                                  (ctype->sexp rtype)
+                                  (sexp:char (ctype->rtype-code rtype))
+                                  (sexp:int (length argtypes))
+                                  (sexp:list (map ctype->sexp argtypes))
+                                  (sexp:list (argnames (length argtypes))))
+                            )))
+           (csig:obj name obtype)
+           -> (let ((ztname (zero-terminate (symbol->string name))))
+                (PUSH forms
+                      (sexp (sexp:symbol 'define)
+                            (sexp:symbol (iface-prefix name))
+                            (sexp (sexp:symbol 'build-ffi-ob)
+                                  (sexp:symbol name)
+                                  (sexp:string ztname)
+                                  (ctype->sexp obtype)
+                                  (sexp:char (ctype->code obtype))
+                                  )))
+                )
+           )))
+
+      ;; (notquiet
+      ;;  (printf "autoffi forms = \n")
+      ;;  (pp (sexp:list forms) 100))
       )
-
-    ;; constant definitions
-    (info.cons::iterate
-     (lambda (k v)
-       (PUSH forms (sexp (sexp:symbol 'define) (sexp:symbol k) (sexp:int v)))))
-
-    ;; bytecode needs a runtime call to ``require-ffi``
-
-    ;; function/variable definitions
-    (info.sigs::iterate
-     (lambda (k v)
-       (match v with
-         (csig:fun name rtype argtypes)
-         -> (let ((ztname (zero-terminate (symbol->string name))))
-              (PUSH forms
-                  (sexp (sexp:symbol 'define)
-                        (sexp:symbol (iface-prefix name))
-                        (sexp (sexp:symbol 'build-ffi-fun)
-                              (sexp:symbol name)
-                              (sexp:string ztname)
-                              (ctype->sexp rtype)
-                              (sexp:char (ctype->rtype-code rtype))
-                              (sexp:int (length argtypes))
-                              (sexp:list (map ctype->sexp argtypes))
-                              (sexp:list (argnames (length argtypes))))
-                        )))
-         (csig:obj name obtype)
-         -> (let ((ztname (zero-terminate (symbol->string name))))
-              (PUSH forms
-                    (sexp (sexp:symbol 'define)
-                          (sexp:symbol (iface-prefix name))
-                          (sexp (sexp:symbol 'build-ffi-ob)
-                                (sexp:symbol name)
-                                (sexp:string ztname)
-                                (ctype->sexp obtype)
-                                (sexp:char (ctype->code obtype))
-                                )))
-              )
-         )))
-    ;; (notquiet
-    ;;  (printf "autoffi forms = \n")
-    ;;  (pp (sexp:list forms) 100))
     (reverse forms)
     ))
