25a32b49254e61a20a02efbc7aaae602f0ea1933
Sam Rushing <gitsam@rushing.nightmare.com>
2017-11-10 15:45:25 -0800

    now called binary+

--- tests/t_map.scm
+++ tests/t_map.scm
@@ -3,5 +3,5 @@
 (include "lib/core.scm")
 (include "lib/pair.scm")
 
-(printn (map (lambda (x) (+ 1 x)) '(0 1 2 3 4)))
-(printn (map2 + '(1 2 3) '(4 5 6)))
+(printn (map (lambda (x) (binary+ 1 x)) '(0 1 2 3 4)))
+(printn (map2 binary+ '(1 2 3) '(4 5 6)))
