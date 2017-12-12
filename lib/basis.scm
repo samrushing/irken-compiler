988b8a451138deb75a9e58a6cba9a14415658d7e
Sam Rushing <gitsam@rushing.nightmare.com>
2017-12-12 00:16:56 -0800

    basis.scm: add stdio.

--- lib/basis.scm
+++ lib/basis.scm
@@ -9,6 +9,7 @@
 (include "lib/queue.scm")
 (include "lib/set.scm")
 (include "lib/alist.scm")
+(include "lib/stdio.scm")
 (include "lib/ctype.scm") ;; needed by os & io.
 (include "lib/lisp_reader.scm") ;; needed by ctype
 (include "lib/os.scm")
