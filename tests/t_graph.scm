44fcf7fe19e05d2ad814eaf7016f57fd9b3aaf85
Sam Rushing <gitsam@rushing.nightmare.com>
2017-12-20 00:51:22 -0800

    updated for lib/graph.scm.

--- tests/t_graph.scm
+++ tests/t_graph.scm
@@ -1,94 +1,39 @@
 ;; -*- Mode: Irken -*-
 
 (include "lib/basis.scm")
-(include "lib/set2.scm")
-(include "lib/alist2.scm")
+(include "lib/map.scm")
+(include "lib/graph.scm")
 
-;; set2 has been removed.  the code in self/graph.scm has been rewritten.
-;; consider rewriting this using the proper set datatypes in lib/set.scm.
-
-(define (transpose g)
-  (let ((gt (alist-maker)))
-    (g::iterate
-     (lambda (k _)
-       (gt::add k (set-maker '()))))
-    (g::iterate
-     (lambda (k vl)
-       (for-each
-	(lambda (v)
-	  (match (gt::get v) with
-	    (maybe:no) -> (gt::add v (set-maker (LIST k)))
-	    (maybe:yes s) -> (s::add k)))
-	(vl::get))))
-    gt))
-
-(define (print-graph g)
-  (print-string "graph = {\n")
-  (g::iterate
-   (lambda (k v)
-     (print-string "  ")
-     (print k)
-     (print-string " ")
-     (printn (v::get))))
-  (print-string "}\n"))
-
-(define (strongly g)
-  (let ((s '())
-	(visited (set-maker '())))
-    (define (visit0 u)
-      (visited::add u)
-      (match (g::get u) with
-	(maybe:no)     -> #u
-	(maybe:yes vl) -> (vl::iterate
-			   (lambda (v)
-			     (if (not (visited::in v))
-				 (visit0 v)))))
-      (PUSH s u))
-    (g::iterate
-     (lambda (u v)
-       (if (not (visited::in u))
-	   (visit0 u))))
-    (let ((gt (transpose g))
-	  (visited (set-maker '()))
-	  (r0 '())
-	  (r1 (set-maker '())))
-      (define (visit1 u)
-	(visited::add u)
-	(match (gt::get u) with
-	  (maybe:no) -> #u
-	  (maybe:yes vl) -> (vl::iterate
-			     (lambda (v)
-			       (if (not (visited::in v))
-				   (visit1 v)))))
-	(r1::add u))
-      (while
-       (not (null? s))
-       (let ((u (pop s)))
-	 (if (not (visited::in u))
-	     (begin
-	       (set! r1 (set-maker '()))
-	       (visit1 u)
-	       (PUSH r0 (r1::get))))))
-      r0)))
+(define (print-graph g krepr)
+  (printf "graph = {\n")
+  (for-map k v g
+    (printf "  " (krepr k) " : " (join krepr ", " (set->list v)) "\n"))
+  (printf "}\n")
+  )
 
 (define test-g
-  '((foo baz)
-    (baz bar)
-    (bar foo)
-    (biff barf)
-    (barf snoo snee)
-    (snoo biff)
+  '((foo baz)        ;; these three
+    (baz bar)        ;; form a
+    (bar foo)        ;; cycle.
+    (biff barf)      ;; as do
+    (barf snoo snee) ;; these
+    (snoo biff)      ;; three.
     (snee)
     (top foo biff)))
 
 (define (make-sample)
-  (let ((g (alist-maker)))
-    (for-each
-     (lambda (l)
-       (g::add (car l) (set-maker (cdr l))))
-     test-g)
+  (let ((g (tree/empty)))
+    (for-list l test-g
+      (tree/insert!
+       g
+       symbol-index-cmp
+       (car l)
+       (list->set (cdr l) symbol-index-cmp (set/empty))))
     g))
 
-(let ((g (make-sample)))
-  (print-graph g)
-  (printn (strongly g)))
+(let ((g0 (make-sample))
+      (g1 (transpose g0 symbol-index-cmp)))
+  (print-graph g0 symbol->string)
+  (print-graph g1 symbol->string)
+  (printn (strongly g0 symbol-index-cmp))
+  )
