fcdbe997ba231506782ad0a8ee3af55f6dc13f0b
Sam Rushing <gitsam@rushing.nightmare.com>
2017-12-30 22:39:17 -0800

    verify generate-random-bits.

--- tests/t_mtwist.scm
+++ tests/t_mtwist.scm
@@ -16,6 +16,7 @@
                1104283350 3804270769 3540304986 1795863450 3766365899 3565204128
                620496131 3556564351))
 
+;; check that we get the correct values.
 (let ((R0 (mt19937 3141)))
   (for-range i 20
     (assert (= (R0) expect0[i])))
@@ -27,3 +28,31 @@
 
 (for x (mt19937-generator 5489 20)
   (printf (zpad 8 (hex x)) "\n"))
+
+;; verify that generate-random-bits creates an identical sequence
+;;   of bits no matter what size pieces we chop it into.
+(define (test-grb)
+  (let ((s0 '())
+        (s1 '())
+        (s2 '()))
+    ;; 7200 bits total
+    (let ((gen32 (generate-random-bits 32 3141)))
+      (for-range i 225
+        (when-maybe x (gen32)
+          (PUSH s0 (format (zpad 8 (hex x)))))))
+    (let ((gen36 (generate-random-bits 36 3141)))
+      (for-range i 200
+        (when-maybe x (gen36)
+          (PUSH s1 (format (zpad 9 (hex x)))))))
+    (let ((gen24 (generate-random-bits 24 3141)))
+      (for-range i 300
+        (when-maybe x (gen24)
+          (PUSH s2 (format (zpad 6 (hex x)))))))
+    (let ((j0 (string-concat (reverse s0)))
+          (j1 (string-concat (reverse s1)))
+          (j2 (string-concat (reverse s2))))
+      (assert (string=? j0 j1))
+      (assert (string=? j1 j2))
+      )))
+
+(test-grb)
