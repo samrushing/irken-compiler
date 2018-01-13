93fe6acf1ef5f1440cf51d7fbb5037d1116f7b41
Sam Rushing <gitsam@rushing.nightmare.com>
2018-01-13 07:43:49 -0800

    rewritten to avoid infinite types.
    The previous version (based on ocaml's) built lists using records.
    New code is based on Okasaki's.

--- lib/queue.scm
+++ lib/queue.scm
@@ -1,48 +1,67 @@
 ;; -*- Mode: Irken -*-
 
-;; based on ocaml/stdlib/queue.ml
+;; okasaki, pure functional.
 
-(define (queue/make)
-  {length = 0 tail = (maybe:no)}
+(datatype fqueue
+  (:t (list 'a) (list 'a))
   )
 
-(define (queue/add q x)
-  (set! q.length (+ 1 q.length))
-  (match q.tail with
-    (maybe:no)
-    -> (let ((cell {content=x next=(magic #u)}))
-	 (set! cell.next cell)
-	 (set! q.tail (maybe:yes cell)))
-    (maybe:yes tail)
-    -> (let ((head tail.next)
-	     (cell {content=x next=head}))
-	 (set! tail.next cell)
-	 (set! q.tail (maybe:yes cell)))))
+(define (fqueue/empty)
+  (fqueue:t '() '()))
 
-;; ocaml raises an exception rather than using maybe
+(define fqueue/empty?
+  (fqueue:t () _) -> #t
+  _               -> #f
+  )
+
+(define fqueue/checkf
+  () b -> (fqueue:t (reverse b) '())
+  f  b -> (fqueue:t f b)
+  )
+
+(define fqueue/snoc
+  x (fqueue:t f b) -> (fqueue/checkf f (list:cons x b))
+  )
+
+(define fqueue/hd
+  (fqueue:t () _)       -> (maybe:no)
+  (fqueue:t (hd . _) _) -> (maybe:yes hd)
+  )
+
+(define fqueue/tl
+  (fqueue:t () _) -> (raise (:Queue/Empty))
+  (fqueue:t f b)  -> (fqueue/checkf (rest f) b)
+  )
+
+(define list->fqueue*
+  ()        acc -> acc
+  (hd . tl) acc -> (list->fqueue* tl (fqueue/snoc hd acc))
+  )
+
+(define (list->fqueue l)
+  (list->fqueue* l (fqueue/empty)))
+
+;; -------- imperative interface -------
+
+(define (queue/make)
+  {len=0 q=(fqueue/empty)})
+
+(define (queue/add! q x)
+  (set! q.q (fqueue/snoc x q.q))
+  (inc! q.len))
+
 (define (queue/peek q)
-  (match q.tail with
-    (maybe:no) -> (maybe:no)
-    (maybe:yes {content=x next=_}) -> (maybe:yes x)))
+  (fqueue/hd q.q))
 
-(define (queue/iterate p q)
-  (match q.tail with
-    (maybe:no) -> #u
-    (maybe:yes tail)
-    -> (let loop ((node tail.next))
-	 (p node.content)
-	 (if (eq? node tail)
-	     #u
-	     (loop node.next)))
+(define (queue/pop! q)
+  (match (fqueue/hd q.q) with
+    (maybe:yes item)
+    -> (begin
+         (set! q.q (fqueue/tl q.q))
+         (dec! q.len)
+         (maybe:yes item))
+    (maybe:no)
+    -> (maybe:no)
     ))
 
-(define (queue/pop q)
-  (match q.tail with
-    (maybe:no) -> (maybe:no)
-    (maybe:yes tail)
-    -> (let ((head tail.next))
-	 (set! q.length (- q.length 1))
-	 (if (= q.length 0)
-	     (set! q.tail (maybe:no))
-	     (set! tail.next head.next))
-	 (maybe:yes head.content))))
+;; todo: queue/iterate (currently not used anywhere).
