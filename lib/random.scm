68b583c1796bcd53feecdecfad51c1744c238869
Sam Rushing <gitsam@rushing.nightmare.com>
2018-01-25 10:54:18 -0800

    use posix ffi.

--- lib/random.scm
+++ lib/random.scm
@@ -1,9 +1,9 @@
 ;; -*- Mode: Irken -*-
 
-;; XXX needs to use libc ffi.
+(require-ffi 'posix)
 
 (define (random)
-  (%%cexp (-> int) "random()"))
+  (posix/random))
 
 (define (srandom n)
-  (%%cexp (int -> undefined) "srandom (%0)" n))
+  (posix/srandom n))
