fbc72ef7d93241169e9e5da0b0b447df88322179
Sam Rushing <gitsam@rushing.nightmare.com>
2018-02-27 17:06:13 -0800

    update c-get/c-set prims.

--- tests/t_halloc.scm
+++ tests/t_halloc.scm
@@ -3,16 +3,16 @@
 (include "lib/basis.scm")
 (include "lib/map.scm")
 
-(let ((buf (%halloc int 20)))
+(let ((buf (halloc int 20)))
   (for-range j 1000000
     (let ((thing (make-string 1024)))
       (for-range i 20
-        (let ((int* (%c-aref int buf i)))
+        (let ((int* (c-aref buf i)))
           ;; (printn int*)
-          (%c-set-int int (+ i 1234) int*)))))
+          (c-set-int int* (+ i 1234))))))
   (for-range i 20
-    (let ((int* (%c-aref int buf i)))
-      (printf (lpad 3 (int i)) " " (lpad 8 (int (%c-get-int int int*))) "\n")
+    (let ((int* (c-aref buf i)))
+      (printf (lpad 3 (int i)) " " (lpad 8 (int (c-get-int int*))) "\n")
       ))
   (let ((thing (halloc int 1)))
     (free thing))
