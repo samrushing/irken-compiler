e256ca8d8f96dfc9dc4cddbbab5805539169bee1
Sam Rushing <gitsam@rushing.nightmare.com>
2018-02-27 17:03:19 -0800

    update c-get/c-set prims.

--- tests/t_endian.scm
+++ tests/t_endian.scm
@@ -7,8 +7,8 @@
   (let ((val0* (halloc u32 1))
         (val1* (%c-cast (array u8) val0*)))
     (define (get n)
-      (%c-get-int u8 (%c-aref u8 val1* n)))
-    (%c-set-int u32 #xf00fda (%c-aref u32 val0* 0))
+      (c-get-int (c-aref val1* n)))
+    (c-set-int (c-aref val0* 0) #xf00fda)
     (match (get 0) (get 1) (get 2) with
       #xda #x0f #xf0 -> #f
       #xf0 #x0f #xda -> #t
