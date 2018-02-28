e4757685171d6e9a713c93db7b5cf01cafa8337f
Sam Rushing <gitsam@rushing.nightmare.com>
2018-02-27 16:39:20 -0800

    c-set/c-get: use new prim wrappers.

--- ffi/t_set_int.scm
+++ ffi/t_set_int.scm
@@ -6,11 +6,11 @@
 (require-ffi 'posix)
 
 (defmacro parray
-  (parray a n w t) ;; array N width type
+  (parray a n w) ;; array N width
   -> (for-range i n
        (printf (repeat w " ")
                (zpad (* w 2)
-                     (hex (%c-get-int t (%c-aref t a i)))))))
+                     (hex (c-get-int (c-aref a i)))))))
 
 (let ((v (halloc u8 40))           ;; u8[40]
       (w (%c-cast (array u16) v))  ;; u16[20]
@@ -18,26 +18,26 @@
 
   ;; 00 01 02 ...
   (for-range i 40
-    (%c-set-int u8 i (%c-aref u8 v i)))
+    (c-set-int (c-aref v i) i))
 
   ;; dump the array in all three interpretations...
-  (parray v 40 1 u8)
+  (parray v 40 1)
   (printf "\n")
-  (parray w 20 2 u16)
+  (parray w 20 2)
   (printf "\n")
-  (parray x 10 4 u32)
+  (parray x 10 4)
   (printf "\n")
 
   ;; set as u32 values...
   (for-range i 10
-    (%c-set-int u32 (>> #xdeadbeef i) (%c-aref u32 x i)))
+    (c-set-int (c-aref x i) (>> #xdeadbeef i)))
 
   ;; dump again
-  (parray v 40 1 u8)
+  (parray v 40 1)
   (printf "\n")
-  (parray w 20 2 u16)
+  (parray w 20 2)
   (printf "\n")
-  (parray x 10 4 u32)
+  (parray x 10 4)
   (printf "\n")
 
   )
