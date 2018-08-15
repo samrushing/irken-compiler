148b8e8540e6bcb37529176b55aee45ce1bcb5df
Sam Rushing <gitsam@rushing.nightmare.com>
2018-08-15 15:23:13 -0700

    vm: include vmffi.scm

--- lib/basis.scm
+++ lib/basis.scm
@@ -4,14 +4,15 @@
 
 (include "lib/core.scm")
 (include "lib/pair.scm")
+(include "lib/symbol.scm")
 (include "lib/string.scm")
 (include "lib/format.scm")
-(include "lib/symbol.scm")
 (include "lib/sexp.scm")
 (include "lib/queue.scm")
 (include "lib/set.scm")
 (include "lib/alist.scm")
 (include "lib/stdio.scm")
+(%backend bytecode (include "lib/vmffi.scm"))
 (include "lib/ctype.scm") ;; needed by os & io.
 (include "lib/lisp_reader.scm") ;; needed by ctype
 (include "lib/os.scm")
