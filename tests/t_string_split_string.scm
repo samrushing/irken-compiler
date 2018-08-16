d7032609411a27cac8c8891c6dd9ee57c9ef6c60
Sam Rushing <gitsam@rushing.nightmare.com>
2018-08-15 19:58:46 -0700

    string-split-string: more tests.

--- tests/t_string_split_string.scm
+++ tests/t_string_split_string.scm
@@ -10,5 +10,12 @@
 (printn (string-split-string "aaa---a---aaa---" "aaa"))
 (printn (string-split-string "aaaaa---a---aaaaa---" "aaa"))
 
+(printn (string-split-string "header0: value0\r\nheader1: value1\r\nheader2: value2\r\n\r\n" "\r\n"))
+(printn (string-split-string "header0: value0\r\nheader1: value1\r\nheader2: value2\r\n" "\r\n"))
+(printn (string-split-string "header0: value0\r\nheader1: value1\r\nheader2: value2" "\r\n"))
+(printn (string-split-string "header0: value0" "\r\n"))
+(printn (string-split-string "header0: value0\r" "\r\n"))
+(printn (string-split-string "header0: value0\r\n" "\r\n"))
+
 (printn (string-replace-all "aaaaa---a---aaaaa---" "aaa" "b"))
 
