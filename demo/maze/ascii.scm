81041f45a3cc08b15e3b3a3cebc02a0396b5c814
Sam Rushing <gitsam@rushing.nightmare.com>
2018-08-24 14:13:11 -0700

    maze/gen: more output formats.

--- demo/maze/ascii.scm
+++ demo/maze/ascii.scm
@@ -85,3 +85,65 @@
     (draw)
     ))
 
+;; --- random output formats ---
+
+;; note: this is still twice as large as it needs to be,
+;;  since there is redundancy in the wall flags. I think
+;;  we can cut this in half.
+(define (graph->hex G m n)
+  (for-range y n
+    (for-range x m
+      (printf (zpad 1 (hex G[(+ x (* m y))]))))
+    (printf "\n")))
+
+(define (bytegen G m n)
+  (makegen emit
+    (let ((byte 0)
+          (bits 0))
+      (for-range y n
+        (for-range x m
+          (let ((wall G[(+ x (* y m))])
+                (v (match (bit-set? wall 3) (bit-set? wall 1) with
+                     #t #t -> 3
+                     #t #f -> 2
+                     #f #t -> 1
+                     #f #f -> 0)))
+            (set! byte (logior (<< byte 2) v))
+            (inc! bits 2)
+            (when (= 8 bits)
+              (emit (int->char byte))
+              (set! byte 0)
+              (set! bits 0))
+            )))
+      (when (> bits 0)
+        (emit (int->char byte)))
+      )))
+
+;; the most compact (ASCII) encoding available.
+;; we use 2 bits per cell, pack into 8-bit
+;; strings and base85-encode.
+
+(define (graph->base85 G m n)
+  (let ((result '()))
+    (for ch (b85-enc (bytegen G m n))
+      (PUSH result ch))
+    (printf (list->string (reverse result)) "\n")
+    ))
+
+;; raw binary, 2 bits per cell.
+;; to use: (graph->bin "/tmp/maze.bin" maze w h)
+(define (graph->bin path G m n)
+  (let ((file (stdio/open-write path))
+        (buffer (make-string 8192))
+        (bytes 0))
+    (for ch (bytegen G m n)
+      (string-set! buffer bytes ch)
+      (inc! bytes)
+      (when (= bytes (string-length buffer))
+        (stdio/write file buffer)
+        (set! bytes 0)))
+    (when (> bytes 0)
+      (stdio/write file (substring buffer 0 bytes))
+      #u
+      )
+    ))
