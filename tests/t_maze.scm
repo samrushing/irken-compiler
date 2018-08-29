b60b3b55faf6baec4d01bdd9ffb19f576d01aff4
Sam Rushing <gitsam@rushing.nightmare.com>
2018-08-29 15:27:16 -0700

    t_maze: use `require` and `posix/random`.

--- tests/t_maze.scm
+++ tests/t_maze.scm
@@ -1,11 +1,9 @@
 ;; -*- Mode: Irken -*-
 
-(include "lib/basis.scm")
-(include "lib/map.scm")
-
+(require "lib/basis.scm")
 (require-ffi 'posix)
 
 ;; https://10print.org/
 ;; 10 PRINT CHR$(205.5+RND(1)); : GOTO 10
 
-(while #t (printf (if (= 0 (mod (posix/rand) 2)) "╱" "╲")))
+(while #t (printf (if (= 0 (mod (posix/random) 2)) "╱" "╲")))
