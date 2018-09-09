83196caade03457cb8eaa29c0e27984f152ca96b
Sam Rushing <gitsam@rushing.nightmare.com>
2018-09-08 23:13:44 -0700

    float: finally working.
    There are still issues:
    Conversion in the presence of a decimal exponent could
      be more efficient (currently we multiply by 1eN).
    A more exact conversion might use bignums directly.
    Would be nice to have a converter for big/big ratios.
    [i.e., a ratio type of big/big could represent exact 'floats']

--- tests/t_float.scm
+++ tests/t_float.scm
@@ -1,32 +1,6 @@
 ;; -*- Mode: Irken -*-
 
-;; can we haz floats?
-;;
-;; boxing is the obvious and less efficient approach.
-;;
-;; can we have immediate floats though?
-;;
-;; [can we safely assume ieee754?]
-;;
-;; my first thought was to use ...10 to represent floats.
-;; this would require shifting all other immediate types
-;; by one, and adjusting a _lot_ of other code.  It would
-;; also have a performance impact, because all immediate-testing
-;; code would need a second branch to test for int, then float, then...
-;;
-;; but what if we could let ints masquerade as floats, knowing that we
-;; lose one bit of the fraction part? (52->51 bits of fraction in a
-;; binary64 float).
-;;
-;; the runtime code (dump_object, etc...) would have no way of telling
-;;  them apart, but that facility is becoming less important.
-;;
-;; the typing system would need a way to cast back and forth.
-;; so %i2f and %f2i.
-;;
-;; we would need a way to create float objects from literals.
-;;
-
+;; immediate floating-point.
 ;; assumes IEEE754 binary64 'double'.
 
 ;; SEEEEEEEEEEEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
@@ -34,21 +8,37 @@
 ;; E := exponent (11 bits)
 ;; F := significand (52 bits)
 ;;
-;; by masquerading as irken integers, we lose one bit of the significand.
+;; by masquerading as irken runtime integers, we lose one bit of the significand.
 ;; [bit zero is always set when stored, always cleared when accessed]
+
+;; Note:
+;; although the runtime (i.e. gc) sees an immediate float as an integer,
+;;   it is not stored shifted like other integers.  This can be confusing when
+;;   looking at the value as an 'integer'.
+;; for example, the encoded value of '1' is:
+;;   0x3ff0000000000000
+;; but appears as
+;;   0x1ff8000000000000
+;; when viewed with %f2i.
+
+;; two new no-op typing prims:
+;; (%f2i #f n) -> changes the type of an int to float.
+;; (%i2f #f n) -> changes the type of a float to int.
 
 (require "lib/basis.scm")
 (require "demo/bignum.scm")
 
+;; XXX write our own printer.
 (define (float->string f)
   (let ((buffer (make-char-buffer 40))
         (r (%%cexp (float int (cref char) -> int)
-                   "snprintf(%2, %1, \"%%e\", %0)"
+                   "snprintf(%2, %1, \"%%.17e\", %0)"
                    f 40 buffer)))
     (if (> r 40)
         (raise (:Snprintf/ResultTooBig r))
         (%cref->string #f buffer r))))
 
+;; this isn't meant for normal computation, instead it is a tool for encoding/decoding floats.
 (datatype ieee754
   (:double bool int int) ;; sign exponent significand (implied high bit of one)
   (:zero bool)           ;; sign
@@ -61,6 +51,7 @@
 (define (bitmask w n)
   (logand n (- (<< 1 w) 1)))
 
+;; decode an immediate float into an ieee754.
 (define (float/decode f)
   (let ((fi (%f2i #f f))
         (neg? (<0 fi))
@@ -76,6 +67,7 @@
       _ _     -> (ieee754:double neg? (- exp 1023) sig)
       )))
 
+;; encode an ieee754 into an immediate float.
 (define float/encode
   (ieee754:double neg? exp sig)
   -> (cond ((or (> exp 2046) (< exp -1022))
@@ -87,8 +79,7 @@
                   (exp0 (+ exp 1023))
                   (r (logior
                       (<< (logior (<< sign 11) exp0) 51)
-                      (bitmask 52 sig))))
-              (printf (zpad 16 (hex r)) "\n")
+                      (>> (bitmask 52 sig) 1)))) ;; tricky - here is where we lose one bit.
               (%i2f #f r)
               )))
   (ieee754:zero neg?)
@@ -101,8 +92,13 @@
   -> (%i2f #f (logior (<< (if neg? #x800 #x000) 51) sig))
   )
 
-(define f0 (float/encode (ieee754:zero #f)))
-(define f1 (float/encode (ieee754:double #f 0 (<< 1 52))))
+;; some exact values
+(define f/0  (float/encode (ieee754:zero #f)))
+(define f/1  (float/encode (ieee754:double #f 0 0)))
+(define f/10 (float/encode (ieee754:double #f 3  #x4000000000000)))
+(define f/∞  (float/encode (ieee754:infinity #f)))
+
+(cinclude "math.h")
 
 (define (f+ a b)
   (%%cexp (float float -> float) "%0 + %1" a b))
@@ -116,33 +112,13 @@
   (%%cexp (float -> float) "sin(%0)" a))
 (define (fcos a)
   (%%cexp (float -> float) "cos(%0)" a))
-
-;; (printf (float (f+ f1 f1)) "\n")
-;; (printf (float (f+ f1 f0)) "\n")
-;; (printf (float (f- f1 f1)) "\n")
-
-(define (t0)
-  (printf "+zero " (float (float/encode (ieee754:zero #f))) "\n")
-  (printf "-zero " (float (float/encode (ieee754:zero #t))) "\n")
-  (printf "+inf  " (float (float/encode (ieee754:infinity #f))) "\n")
-  (printf "-inf  " (float (float/encode (ieee754:infinity #t))) "\n")
-  (printf "+NaN  " (float (float/encode (ieee754:nan #f 42))) "\n")
-  (printf "-NaN  " (float (float/encode (ieee754:nan #t 42))) "\n")
-  (printf "subn  " (float (float/encode (ieee754:subnormal #f 42))) "\n")
-  )
-
-;;(t0)
-
-;;(printf "sin(pi/4) " (float
-
-(define (highest-bit-set n)
-  (%backend c (%%cexp (int -> int) "(64 - __builtin_clzl (%0))" n))
-  (%backend llvm (- 64 (%llvm-call ("@irk_ctlz" (int -> int)) n)))
-  (%backend bytecode
-    (let loop ((n n) (r 0))
-      (if (zero? n)
-          r
-          (loop (>> n 1) (+ r 1))))))
+(define (fpow a b)
+  (%%cexp (float -> float) "pow(%0,%1)" a b))
+;; XXX not portable.
+;; (define (fexp10 a)
+;;   (%%cexp (float -> float) "__exp10(%0)" a))
+(define (fexp10 a)
+  (%%cexp (float -> float) "pow(10,%0)" a))
 
 ;; ---- float parsing ----
 
@@ -192,109 +168,115 @@
     acc (dig . tl) -> (digits->big (big (+ (* big/10 acc) (I dig))) tl)
     )
 
-  ;; ((list int) (list int) int) -> (:tuple (list int) (list int) int)
-  (define move-dot
-    whole frac 0 -> (:tuple whole frac 0)
-    whole frac n
-    -> (let ((lw (length whole))
-             (lf (length frac))
-             (m (if (>0 n) (min lf n) (min lw (- n)))))
-         ;; adjust the position of the dot
-         ;;   by moving digits from whole <-> frac.
-         ;; wwww.ffff E+1 => wwwwf.fff
-         ;; wwww.ffff E+2 => wwwwff.ff
-         ;; wwww.ffff E-1 => www.wffff
-         ;; wwww.ffff E-2 => ww.wwffff
-         (cond ((>0 n) (:tuple (append whole (take frac m)) (drop frac m) (- n m)))
-               ((<0 n) (:tuple (take whole (- lw m)) (append (drop whole (- lw m)) frac) (+ n m)))
-               (else   (:tuple whole frac 0)))))
+  (define (clamp n)
+    (let ((nbits (big->bits n)))
+      (if (> nbits 52)
+          (clamp (big-quo n big/10))
+          (:tuple (big->int (big-lshift n (- 52 nbits))) (- nbits 1)))))
 
-  ;;  x       y
-  ;; ---  = ----
-  ;; 10^e   2^52
-
-  (define (frac->big digits)
-    (let ((n (digits->big big/0 digits))
-          (ndigits (length digits))
-          (e10 (big-pow big/10 ndigits))
-          (e2 (int->big (<< 1 52))))
-      (big-quo (big-mul e2 n) e10)))
+  (define (normalize n)
+    (logxor n (<< 1 51)))
 
   (let ((chars (string->list s))
         ((neg? (whole chars)) (read-signed-digits chars))
         ((frac chars) (read-fraction chars))
         ((eneg? (exp chars)) (read-exponent chars))
-        (exp0 (if eneg? (- (digits->int 0 exp)) (digits->int 0 exp)))
-        ((whole frac exp) (move-dot whole frac exp0))
-        (whole0 (digits->big big/0 whole))
-        (wbits (big->bits whole0))
-        (fbits (- 52 wbits))
-        (joined (big-add (big-lshift whole0 52) (frac->big frac))))
-    (printf "adjusted " (big-repr joined) "\n")
-
-    ;; ok that works all fine and dandy, _except_ we now have an
-    ;;  unwanted decimal exponent to deal with.  so maybe we _do_
-    ;;  have to join whole & frac first, then convert to binary,
-    ;;  taking the exponent into account.
+        (joined (digits->big big/0 (append whole frac)))
+        ((n52 bexp) (clamp joined))
+        (dexp0 (digits->int 0 exp))
+        (dexp1 (if eneg? (- dexp0) dexp0))
+        (dexp (- dexp1 (length frac)))
+        (normal (normalize n52)))
+    ;; (printf "     s " s "\n"
+    ;;         "joined " (big-repr joined) "\n"
+    ;;         "   n52 " (int n52) "\n"
+    ;;         "   n52 " (hex n52) "\n"
+    ;;         "  bexp " (int bexp) "\n"
+    ;;         " dexp0 " (int dexp0) "\n"
+    ;;         " dexp1 " (int dexp1) "\n"
+    ;;         "  dexp " (int dexp) "\n"
+    ;;         "normal " (zpad 13 (hex normal)) "\n")
+    (let ((result (float/encode (ieee754:double neg? bexp (<< normal 1)))))
+      (when (not (zero? dexp))
+        ;; we have a decimal exponent to deal with
+        (set! result (f* result (fexp10 (int->float dexp)))))
+      result)
     ))
 
-;; ok, converting to binary.
-;; 1) split into whole and fraction parts.
-;;  note: there's no way to avoid this, since we are in base 10 but
-;;    need the mantissa in binary.  for example, 3.14 and 31.4 have
-;;    different mantissas.
+(define (big->float n)
 
-;; generate the ratio 16 bits at a time.
-(define (ratio-bit-gen n d)
-  (set! d (big-quo d (int->big (<< 1 16))))
-  (makegen emit
-    (let loop ((n n))
-      (let (((q r) (big-div n d)))
-        (emit q)
-        (loop (big-lshift r 16))))))
+  (define (clamp n)
+    (let ((nbits (big->bits n)))
+      (if (> nbits 52)
+          (clamp (big-quo n big/10))
+          (:tuple (big->int (big-lshift n (- 52 nbits))) (- nbits 1)))))
 
-;; ;; too many digits, needs bignum:
-;; (printn (string->dec-ratio "3.14159265358979323846"))
-;; ;; just fits:
-;; (printn (string->dec-ratio "3.14159265358979323"))
+  (define (normalize n)
+    (logxor n (<< 1 51)))
 
-;; 400921fb54442d18
-;; 0 100 0000 0000
-;; exp = 0x400
-;; actual exp
-;;     = 0x400 - 0x3ff = 1
-;; sig = 921fb54442d18
-;;     = 1001 0010 0001 1111 1011 0101 0100 0100 0100 0010 1101 0001 1000
-;; actual sig
-;;     = 0001 1001 0010 0001 1111 1011 0101 0100 0100 0100 0010 1101 0001 1000
-;;     = 1921fb54442d18
-;;     = 0b11001001000011111101101010100010001000010110100011000
-;; with exp
-;;     = 0b11 0b001001000011111101101010100010001000010110100011000
+  (define (encode n neg?)
+    (let (((n52 bexp) (clamp n))
+          (normal (normalize n52)))
+    (float/encode (ieee754:double neg? bexp (<< normal 1)))))
 
-;; "314159265358979323846"
-;; "1000000000000000000000"
-;; 300100100001111110110101010001000100001011010001100001000110
+  (match n with
+    (big:zero)  -> f/0
+    (big:pos _) -> (encode n #f)
+    (big:neg _) -> (encode (big-negate n) #t))
+  )
 
+(define (int->float n)
+  (big->float (int->big n)))
 
-;;    3 0010 0100 0011 1111 0110 1010 1000 1000 1000 0101 1010 0011
-;; 0011 0010 0100 0011 1111 0110 1010 1000 1000 1000 0101 1010 0011
-;;  3    2    4    3    f    6    a    8    8    8    5    a    3
+;; ---- testing ----
 
-;;3243f6a8885a3
+(define (t0)
+  (printf "+zero " (float (float/encode (ieee754:zero #f))) "\n")
+  (printf "-zero " (float (float/encode (ieee754:zero #t))) "\n")
+  (printf "+inf  " (float (float/encode (ieee754:infinity #f))) "\n")
+  (printf "-inf  " (float (float/encode (ieee754:infinity #t))) "\n")
+  (printf "+NaN  " (float (float/encode (ieee754:nan #f 42))) "\n")
+  (printf "-NaN  " (float (float/encode (ieee754:nan #t 42))) "\n")
+  (printf "subn  " (float (float/encode (ieee754:subnormal #f 42))) "\n")
+  )
 
-(parse-float "3.14159265358979323846")
+(define (pfloat f)
+  (printf (float f) "\n"))
 
+(define (t1)
+  (pfloat (parse-float "3"))
+  (pfloat (parse-float "1234"))
+  (pfloat (parse-float "1234.5"))
+  (pfloat (parse-float "3.14159265358979323846"))
+  (pfloat (parse-float "1e10"))
+  (pfloat (parse-float "1e-5"))
+  (pfloat (parse-float "3.14159e10"))
+  (pfloat (parse-float "-3.14159"))
+  (pfloat (parse-float "3.14159e1"))
+  (pfloat (parse-float "3.14159e-1"))
+  (pfloat (parse-float "3.14159e+1"))
+  (pfloat (parse-float "7"))
+  (pfloat (parse-float "7e20"))
+  (pfloat (parse-float "7e-1"))
+  (pfloat (parse-float "2718281828e-4"))
+  (pfloat (parse-float "2718281828e+4"))
+  (pfloat (parse-float "2718.281828e-4"))
+  (pfloat (parse-float "2718.281828e+4"))
+  )
 
-;; (parse-float "3.14159e10")
-;; (parse-float "-3.14159")
-;; (parse-float "3.14159e1")
-;; (parse-float "3.14159e-1")
-;; (parse-float "3.14159e+1")
-;; (parse-float "7")
-;; (parse-float "7e20")
-;; (parse-float "7e-1")
-;; (parse-float "2718281828e-4")
-;; (parse-float "2718281828e+4")
-;; (parse-float "2718.281828e-4")
-;; (parse-float "2718.281828e+4")
+(define (t2)
+  (pfloat (f* f/∞ f/∞))
+  (pfloat (float/encode (ieee754:double #f 1 #x8000000000000)))
+  (pfloat (int->float 3))
+  (pfloat (big->float (big-lshift (int->big 1) 400)))
+  (pfloat (fexp10 (int->float 3)))
+  (pfloat (parse-float "1234.567891234"))
+  (pfloat (int->float -3))
+  )
+
+(t0)
+(t1)
+(t2)
+
+(pfloat (int->float 1))
+(printf (zpad 16 (hex (%f2i #f (int->float 1)))) "\n")
